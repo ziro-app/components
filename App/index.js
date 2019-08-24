8aaadb3456bd199fdf74a81aaab2b0c817bc9410
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-08-24 10:47:12 -0300

    add container component

---
+++ App/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { InputTextType1 } from '../InputTextType1/index'
+import { container } from './styles'
+
+export const App = () =>
+	<div style={container}>
+		<InputTextType1 />
+	</div>
--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { App } from './App/index'
 import './index.css'
-import { InputTextType1 } from './InputTextType1/index'
-
-const App = () => 
-	<div>
-		<InputTextType1 />
-	</div>
 
 render(<App />, document.getElementById('app'))
