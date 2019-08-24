48c08db90f2b0030d374c463ab57c19ae4a3b96a
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-08-24 14:33:13 -0300

    fix exporting issue

--- src/components/InputTextType1/index.js
+++ src/components/InputTextType1/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { input } from './styles'
 
-export const InputTextType1 = () => {
+const InputTextType1 = () => {
 	return (
 		<input
 			style={input}
@@ -14,3 +14,5 @@ export const InputTextType1 = () => {
 		/>
 	)
 }
+
+export default InputTextType1
