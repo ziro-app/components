5498fd3aba2c2223687aa1aaf35224f40d5a4936
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-09-02 14:11:56 -0300

    add function to display different components based on state

--- src/components/EditableData/index.js
+++ src/components/EditableData/index.js
@@ -5,12 +5,21 @@ import InputText from '../InputText/index'
 import BadgeValidated from '../BadgeValidated/index'
 
 const EditableData = () => {
+	const display = {
+		idle: <PenIcon size={13} />,
+		editing: <div style={submit} onClick={saveFname}>Salvar</div>,
+		submitting: <Spinner size={'2rem'} style={spinner} />,
+		success: <PenIcon size={13} />,
+		error: <div style={submit} onClick={saveFname}>Salvar</div>
+	}
 	return (
 		<div>
 			<InputNotice />
 			<InputLabel
 				hasBadge={true}
 				badge={<BadgeValidated fontSize={9} />}
+				hasUiState={true}
+				displayUiState={display['editing']}
 			/>
 			<InputText />
 		</div>
--- src/components/InputLabel/index.js
+++ src/components/InputLabel/index.js
@@ -3,16 +3,18 @@ import PropTypes from 'prop-types'
 import { header, name } from './styles'
 
 
-const InputLabel = ({ hasBadge, badge }) =>
+const InputLabel = ({ hasBadge, badge, hasUiState, displayUiState }) =>
 	<div style={header(hasBadge)}>
 		<label style={name}>Nome</label>
 		{hasBadge && badge}
-		
+		{hasUiState && displayUiState}
 	</div>
 
 InputLabel.propTypes = {
 	hasBadge: PropTypes.bool.isRequired,
-	badge: PropTypes.element.isRequired
+	badge: PropTypes.element.isRequired,
+	hasUiState: PropTypes.bool.isRequired,
+	displayUiState: PropTypes.element.isRequired
 }
 
 export default InputLabel
