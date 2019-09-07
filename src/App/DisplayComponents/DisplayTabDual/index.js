c9ede117fe66de0a8b4bcf13dc56eac7f4d469f2
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-09-07 13:51:53 -0300

    update styles

--- src/App/DisplayComponents/DisplayTabDual/index.js
+++ src/App/DisplayComponents/DisplayTabDual/index.js
@@ -1,5 +1,4 @@
-import React, { Fragment } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 import TabDualTransition from '../../../components/TabDualTransition/index'
 import TabDualHeader from '../../../components/TabDualHeader/index'
 import { container, component } from './styles'
