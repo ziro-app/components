09acb8c5bc7b4d818705c70de3a3a67167663e3a
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-09-08 20:17:35 -0300

    add animation library

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSpring, animated } from 'react-spring'
+import { container } from './styles'
 
 const Drawer = () => {
+	const [isOpen, setIsOpen] = useState(true)
+	const props = useSpring({ transform: `translateX(${isOpen ? '0%' : '-100%'})` })
 	return (
-		<div>Drawer</div>	
+		<animated.div style={{...container, ...props}}>
+			Drawer
+		</animated.div>
 	)
 }
 
---
+++ src/components/Drawer/styles.js
@@ -0,0 +1,7 @@
+export const
+
+container = {
+	width: '100%',
+	height: '100vh',
+	boxSizing: 'border-box'
+}
