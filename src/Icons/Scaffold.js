c5e498f9b706e17a90752b6b3638b9ee4d512a99
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-09-09 08:19:34 -0500

    add Scaffold component to create standard svg components

---
+++ src/Icons/Scaffold.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { primaryColor } from '../../Theme/variables'
+
+export const Scaffold = ({ svgPath, style, onClick, size, color, strokeWidth, onClick, style }) =>
+	<svg
+		style={style}
+		onClick={onClick}
+		width={size}
+		height={size}
+		stroke={color}
+		strokeWidth={strokeWidth}
+		viewBox='0 0 24 24'
+		fill='none'
+		strokeLinecap='round'
+		strokeLinejoin='round'
+	>
+		{svgPath}
+	</svg>
+
+Scaffold.defaultProps = {
+	style: null,
+	onClick: null,
+	size: 24,
+	color: primaryColor,
+	strokeWidth: 2
+}
+
+Scaffold.propTypes = {
+	svgPath: PropTypes.oneOfType([
+		PropTypes.element,
+		PropTypes.arrayOf(PropTypes.element)
+	]).isRequired,
+	style: PropTypes.object,
+	onClick: PropTypes.func,
+	size: PropTypes.number,
+	color: PropTypes.string,
+	strokeWidth: PropTypes.number
+}
