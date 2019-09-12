65422cf02ad6c6378603239d54b414e9cc2a9d86
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-09-11 20:44:41 -0700

    add some styles to panel

--- src/components/DrawerPanel/index.js
+++ src/components/DrawerPanel/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
-// import { useSpring, animated } from 'react-spring'
+import { Link } from 'wouter'
 import Icon from '../Icon/index'
 // import { animation } from './animation'
-import { container } from './styles'
+import { container, user, navlink, icon, text } from './styles'
 
 const DrawerPanel = () => {
 	return (
 		<div style={container}>
-			Panel
+			<div style={user}>
+				<Icon type='close' />
+				<label>Olá, Vitor Barbosa</label>
+				<label>CNPJ: 28.026.371/0001-61</label>
+			</div>
+			<div>
+				<Link style={navlink}>
+					<div style={icon}><Icon type='pen' size={12} /></div>
+					<label style={text}>Atendimentos</label>
+				</Link>
+			</div>
 		</div>
 	)
 }
--- src/components/DrawerPanel/styles.js
+++ src/components/DrawerPanel/styles.js
@@ -1,7 +1,35 @@
+import { fontTitle, primaryColor, grayColor2 } from '../../Theme/variables'
+
 export const
 
 container = {
 	height: '100vh',
 	boxSizing: 'border-box',
 	padding: '20px 10% 0'
+},
+
+user = {},
+
+navlink = {
+	display: 'grid',
+	gridTemplateColumns: 'auto 1fr',
+	gridColumnGap: '8px',
+	alignItems: 'center'
+},
+
+icon = {
+	display: 'grid',
+	placeItems: 'center',
+	width: '24px',
+	height: '24px',
+	borderRadius: '100%',
+	boxShadow: `0px 3px 11px -4px rgba(34,34,34,0.7)`
+},
+
+text = {
+	fontFamily: fontTitle,
+	fontSize: '1.5rem',
+	fontWeight: '600',
+	textTransform: 'uppercase',
+	color: primaryColor
 }
