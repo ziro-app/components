fc0a09a05ebdb031fad574308487bc0ab9dfd420
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-09-21 21:25:37 -0700

    fix svg

--- src/App/DisplayComponents/DisplayCheckout/index.js
+++ src/App/DisplayComponents/DisplayCheckout/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Checkout from '../../../components/Checkout/index'
+import Icon from '../../../components/Icon/index'
 import { container } from '../../../Theme/variables'
 
 export const DisplayCheckout = () =>
 	<div style={container}>
-		<Checkout
-			
-		/>
+		<Checkout />
+		<Icon type='amex' />
+		<Icon type='visa' />
+		<Icon type='mastercard' />
+		<Icon type='elo' />
 	</div>
