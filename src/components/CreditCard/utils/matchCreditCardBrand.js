22f8c27768c31dfc60259d606bf1933257196f56
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-09-24 17:27:43 -0700

    add function to match credit card

---
+++ src/components/CreditCard/utils/matchCreditCardBrand.js
@@ -0,0 +1,17 @@
+import { brands } from './brands'
+
+const matchCreditCardBrand = () => {
+	const match = brands.filter(brand =>
+		!!brand[1].map(pattern => cardNumber.startsWith(pattern)).filter(value => !!value).pop()
+	)
+	/* matching algorithm conflicts Visa and Elo. In all cases of conflict must return Elo */
+	if (match.length > 1 && match[0][1] < match[1][1]) {
+		const [[result]] = match.filter(name => name[0] === 'elo')
+		return result
+	}
+	if (match && match.length) {
+		const [[result]] = match
+		return result
+	}
+	return ''
+}
