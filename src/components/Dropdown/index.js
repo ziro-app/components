308100849e3319fc4afb3794b010480f58000043
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-09-30 17:23:07 -0700

    fix styles

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,19 +1,20 @@
 import React, { useState, Fragment } from 'react'
 import { motion } from 'framer-motion'
 import InputText from '../InputText'
-import { data } from './styles'
+import { modal, data } from './styles'
 
 const Dropdown = () => {
 	const [isOpen, setIsOpen] = useState(false)
 	return (
 		<Fragment>
 			<InputText onFocus={() => setIsOpen(true)} onBlur={() => setIsOpen(false)} />
-			{isOpen && <motion.div style={data} initial={{ opacity: 0, height: '120px' }} animate={{ opacity: 1, height: '150px' }} transition={{ type: "spring", stiffness: 1500, damping: 70 }}>
-				<label>Option 1</label>
-				<label>Option 1</label>
-				<label>Option 1</label>
-				<label>Option 1</label>
-				<label>Option 1</label>
+			{isOpen && <motion.div style={modal} initial={{ opacity: 0, height: '0' }} animate={{ opacity: 1, height: '120px' }}>
+				<label style={data}>1x 100.000,00 = R$100.000,00</label>
+				<label style={data}>2x 50.000,00 = R$100.000,00</label>
+				<label style={data}>3x 33.333,33 = R$100.000,00</label>
+				<label style={data}>4x 25.000,00 = R$100.000,00</label>
+				<label style={data}>5x 20.000,00 = R$100.000,00</label>
+				<label style={data}>6x 16.666,66 = R$100.000,00</label>
 			</motion.div>}
 		</Fragment>
 	)
--- src/components/Dropdown/styles.js
+++ src/components/Dropdown/styles.js
@@ -1,17 +1,24 @@
-import { fontBody, fontSizeInput, primaryColor, grayColor1, grayColor2, grayColor3 } from '../../Theme/variables'
+import { fontBody, fontSizeInput, fontSizeSmall, primaryColor, grayColor1, grayColor2, grayColor3 } from '../../Theme/variables'
 
-export const data = {
+export const
+
+modal = {
 	display: 'grid',
 	overflowY: 'scroll',
 	boxSizing: 'border-box',
 	width: '100%',
-	margin: '3px 0 0',
-	padding: '8px 22px',
+	maxHeight: '120px',
+	margin: '1px auto 0',
+	padding: '10px 20px',
 	border: `2px solid ${grayColor1}`,
 	borderRadius: '8px',
 	fontFamily: `${fontBody}, 'system-ui', 'sans-serif'`,
-	fontSize: fontSizeInput,
 	color: primaryColor,
 	backgroundColor: '#FDFDFD',
 	boxShadow: `rgba(34,34,34,0.3) 0px 3px 10px -3px`
+},
+
+data = {
+	padding: '5px 0',
+	fontSize: fontSizeInput
 }
