5c7e72d62d3d97b2641a2f7547f633e72e590ba4
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-10-05 20:12:32 -0700

    add more error validations

--- src/components/Checkout/utils/validateForm.js
+++ src/components/Checkout/utils/validateForm.js
@@ -1,7 +1,12 @@
 export const validateForm = state => {
 	const [number, cardholder, expiry, cvv, cpf, installments] = state
-	const numberIsValid = number.replace(/\s/g, '')
-	console.log(numberIsValid)
-	const formIsValid = !!numberIsValid
+	const numberIsValid = number.replace(/\s/g, '').length >= 12
+	const cardholderIsValid = !!cardholder
+	const expiryIsValid = !!expiry && expiry.length === 5
+	const cvvIsValid = !!cvv && cvv.length === 4
+	const cpfIsValid = !!cpf && cpf.length === 14
+	const installmentsIsValid = !!installments
+	const formIsValid = numberIsValid && cardholderIsValid && expiryIsValid
+		&& cvvIsValid && cpfIsValid && installmentsIsValid
 	return formIsValid
 }
