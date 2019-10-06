e3b5c3d028a765426de6ba5f9b2c2756e61bcce8
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-10-05 19:55:27 -0700

    refactor useCard hook

--- src/components/Checkout/utils/useCard.js
+++ src/components/Checkout/utils/useCard.js
@@ -5,20 +5,24 @@ import { matchCreditCardBrand } from './matchCreditCardBrand'
 export const useCard = number => {
 	const [brand, setBrand] = useState('')
 	const [numberMaskedCard, setNumberMaskedCard] = useState('')
-	// const [numberMaskedInput, setNumberMaskedInput] = useState('')
-	let numberMaskedInput = number => maskInput(number, '#### #### #### ####', true)
+	let numberMaskedInput
+	switch (true) {
+		case /^amex$/.test(brand):
+			numberMaskedInput = number => maskInput(number, '#### ###### #####', true)
+			break;
+		case /^visa$|^mastercard$/.test(brand):
+			numberMaskedInput = number => maskInput(number, '#### #### #### #######', true)
+			break;
+		default:
+			numberMaskedInput = number => maskInput(number, '#### #### #### ####', true)
+			break;
+	}
 	const expiryMasked = expiry => maskInput(expiry, '##/##', true)
 	const cvvMasked = cvv => maskInput(cvv, '####', true)
 	const cpfMasked = cpf => maskInput(cpf, '###.###.###-##', true)
 	useEffect(() => {
 		setBrand(matchCreditCardBrand(number.replace(/\s/g, '')))
 		setNumberMaskedCard(number.replace(/\s/g, ''))
-		if (brand === 'amex')
-			numberMaskedInput = number => maskInput(number, '#### ###### #####', true)
-		else if (brand === 'visa' || brand === 'mastercard')
-			numberMaskedInput = number => maskInput(number, '#### #### #### #######', true)
-		else
-			numberMaskedInput = number => maskInput(number, '#### #### #### ####', true)
 	}, [number])
 	
 	return [brand, numberMaskedCard, numberMaskedInput, expiryMasked, cvvMasked, cpfMasked]
