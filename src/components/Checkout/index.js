df0d5dc3a548b1be5936615a0a0bae5e37ff6fd5
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-10-13 11:06:17 -0700

    start refactoring checkout component

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -1,17 +1,16 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { useCard } from './utils/useCard'
-import { useForm } from './utils/useForm'
 import { installmentOptions } from './utils/installmentUtils'
+// import { validations } from './utils/validations'
 import Header from '../Header/index'
 import CreditCard from '../CreditCard/index'
+import Form from '../Form/index'
+import FormInput from '../FormInput/index'
 import InputText from '../InputText/index'
-import InputLabel from '../InputLabel/index'
 import Dropdown from '../Dropdown/index'
-import Icon from '../Icon/index'
 import { Footer } from './Footer/index'
 import { Submit } from './Submit/index'
-import { alertColor } from '../../Theme/variables'
 import { container, labelHeader, dual, errorBlock, errorMsg } from './styles'
 
 const Checkout = ({ charge, maxInstallments, seller }) => {
@@ -22,116 +21,131 @@ const Checkout = ({ charge, maxInstallments, seller }) => {
 	const [cpf, setCpf] = useState('')
 	const [installments, setInstallments] = useState('')
 	const [modal, setModal] = useState(false)
-	const state = [number, cardholder, expiry, cvv, cpf, installments]
 	const [brand, numberMaskedCard, numberMaskedInput, expiryMasked, cvvMasked, cpfMasked] = useCard(number)
-	const [hasError, errors, submitting, submitForm, submitError] = useForm(state, setModal)
+	const validations = [
+		{
+			name: 'number',
+			validation: value => !!value && value.replace(/\s/g, '').length >= 12,
+			value: number,
+			message: 'Verifique o número digitado'
+		},
+		{
+			name: 'cardholder',
+			validation: value => !!value,
+			value: cardholder,
+			message: 'Campo obrigatório'
+		},
+		{
+			name: 'expiry',
+			validation: value => !!value && value.length === 5,
+			value: expiry,
+			message: 'Verifique a validade'
+		},
+		{
+			name: 'cvv',
+			validation: value => !!value && value.length >= 3,
+			value: cvv,
+			message: 'Verifique o código atrás do cartão'
+		},
+		{
+			name: 'cpf',
+			validation: value => !!value && value.length === 14,
+			value: cpf,
+			message: 'Deve conter 11 dígitos'
+		},
+		{
+			name: 'installments',
+			validation: value => !!value,
+			value: installments,
+			message: 'Campo obrigatório'
+		}
+	]
 	return (
-		<form onSubmit={submitting ? e => e.preventDefault() : submitForm}>
+		<>
 			<Header type='title-only' title='Pagamento' />
-			<div style={container}>
-				<CreditCard
-					number={numberMaskedCard}
-					brand={brand}
-					cardholder={cardholder}
-					expiry={expiry}
-					cvv={cvv}
-				/>
-				<div>
-					<InputLabel
-						name='Número do cartão'
-						styleHeader={labelHeader}
-						badge={<Icon type='alert' size={13} strokeWidth={3} color={alertColor} />}
-						hasBadge={!!hasError && !errors.numberIsValid}
-					/>
-					<InputText
-						value={number}
-						onChange={({ target: { value } }) => setNumber(numberMaskedInput(value))}
-						submitting={submitting}
-						placeholder='1234 1234 1234 1234'
-					/>
-				</div>
-				<div>
-					<InputLabel
-						name='Titular do cartão'
-						styleHeader={labelHeader}
-						badge={<Icon type='alert' size={13} strokeWidth={3} color={alertColor} />}
-						hasBadge={!!hasError && !errors.cardholderIsValid}
-					/>
-					<InputText
-						value={cardholder}
-						onChange={({ target: { value } }) => setCardholder(value)}
-						submitting={submitting}
-						placeholder='Fernando(a) da Silva'
-					/>
-				</div>
-				<div style={dual}>
-					<div>
-						<InputLabel
-							name='Validade'
-							styleHeader={labelHeader}
-							badge={<Icon type='alert' size={13} strokeWidth={3} color={alertColor} />}
-							hasBadge={!!hasError && !errors.expiryIsValid}
-						/>
-						<InputText
-							value={expiry}
-							onChange={({ target: { value } }) => setExpiry(expiryMasked(value))}
-							submitting={submitting}
-							placeholder='01/24'
-						/>
-					</div>
-					<div>
-						<InputLabel
-							name='CVV'
-							styleHeader={labelHeader}
-							badge={<Icon type='alert' size={13} strokeWidth={3} color={alertColor} />}
-							hasBadge={!!hasError && !errors.cvvIsValid}
-						/>
-						<InputText
-							value={cvv}
-							onChange={({ target: { value } }) => setCvv(cvvMasked(value))}
-							submitting={submitting}
-							placeholder='1111'
-						/>
-					</div>
-				</div>
-				<div>
-					<InputLabel
-						name='CPF do titular'
-						styleHeader={labelHeader}
-						badge={<Icon type='alert' size={13} strokeWidth={3} color={alertColor} />}
-						hasBadge={!!hasError && !errors.cpfIsValid}
-					/>
-					<InputText
-						value={cpf}
-						onChange={({ target: { value } }) => setCpf(cpfMasked(value))}
-						submitting={submitting}
-						placeholder='111.222.333-44'
-					/>
-				</div>
-				<div>
-					<InputLabel
-						name='Parcelamento'
-						styleHeader={labelHeader}
-						badge={<Icon type='alert' size={13} strokeWidth={3} color={alertColor} />}
-						hasBadge={!!hasError && !errors.installmentsIsValid}
-					/>
-					<Dropdown
-						value={installments}
-						onChange={({ target: { value } }) => setInstallments(value.substring(0,1))}
-						list={installmentOptions(charge, maxInstallments)}
-						submitting={submitting}
-						placeholder='Escolha quantas parcelas'
-					/>
-				</div>
-			</div>
-			<label style={errorBlock}>
-				<div style={errorMsg}>
-					{!!hasError && <Icon type='alert' size={13} strokeWidth={3} color={alertColor} />}&nbsp;{hasError}
-				</div>
-			</label>
-			<Footer charge={charge} installments={installments} seller={seller} submitting={submitting} />
-			<Submit submitting={submitting} modal={modal} setModal={setModal} error={submitError} />
-		</form>
+			<CreditCard
+				number={numberMaskedCard}
+				brand={brand}
+				cardholder={cardholder}
+				expiry={expiry}
+				cvv={cvv}
+			/>
+			<Form
+				buttonName='Confirmar'
+				validations={validations}
+				sendToBackend={() => new Promise((resolve, reject) => setTimeout(() => resolve('OK'), 1000))}
+				inputs={[
+					<FormInput
+						name='number'
+						label='Número do cartão'
+						input={
+							<InputText
+								value={number}
+								onChange={({ target: { value } }) => setNumber(numberMaskedInput(value))}
+								placeholder='1234 1234 1234 1234'
+							/>
+						}
+					/>,
+					<FormInput
+						name='cardholder'
+						label='Titular do cartão'
+						input={
+							<InputText
+								value={cardholder}
+								onChange={({ target: { value } }) => setCardholder(value)}
+								placeholder='Fernando(a) da Silva'
+							/>
+						}
+					/>,
+					<FormInput
+						name='expiry'
+						label='Validade'
+						input={
+							<InputText
+								value={expiry}
+								onChange={({ target: { value } }) => setExpiry(expiryMasked(value))}
+								placeholder='01/24'
+							/>
+						}
+					/>,
+					<FormInput
+						name='cvv'
+						label='CVV'
+						input={
+							<InputText
+								value={cvv}
+								onChange={({ target: { value } }) => setCvv(cvvMasked(value))}
+								placeholder='1111'
+							/>
+						}
+					/>,
+					<FormInput
+						name='cpf'
+						label='CPF do titular'
+						input={
+							<InputText
+								value={cpf}
+								onChange={({ target: { value } }) => setCpf(cpfMasked(value))}
+								placeholder='111.222.333-44'
+							/>
+						}
+					/>,
+					<FormInput
+						name='installments'
+						label='Parcelamento'
+						input={
+							<Dropdown
+								value={installments}
+								onChange={({ target: { value } }) => setInstallments(value.substring(0,1))}
+								list={installmentOptions(charge, maxInstallments)}
+								placeholder='Escolha quantas parcelas'
+							/>
+						}
+					/>,
+				]}
+			/>
+
+		</>
 	)
 }
 
