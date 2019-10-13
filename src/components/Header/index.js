d8f8e0f29fa4975920a623698803c6e060481420
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-10-13 09:35:59 -0700

    finish new Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,18 +8,18 @@ const Header = ({ type, title, icon, setIsOpen, navigateTo }) => {
 	const component = {
 		'icon': 
 			<>
-				<Icon type={icon} style={svg} onClick={setIsOpen} />
-				<h1 style={text}>{title}</h1>
+				<Icon type={icon || 'ziro'} style={svg} onClick={setIsOpen} />
+				<h1 style={text(false)}>{title}</h1>
 			</>,
 		'icon-link':
 			<>
-				<Link to={navigateTo}><Icon type={icon} style={svg} /></Link>
-				<h1 style={text}>{title}</h1>
+				<Link to={navigateTo}><Icon type={icon || 'ziro'} style={svg} /></Link>
+				<h1 style={text(false)}>{title}</h1>
 			</>,
-		'title-only': <h1 style={text}>{title}</h1>
+		'title-only': <h1 style={text(true)}>{title}</h1>
 	}
 	return (
-		<div style={container}>
+		<div style={container(type === 'title-only')}>
 			{component[type]}
 		</div>
 	)
