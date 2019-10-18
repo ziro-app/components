645df105f7300fe6402413de09de462a3486d8b4
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-10-18 16:50:28 -0700

    add illustration

--- src/components/Illustration/index.js
+++ src/components/Illustration/index.js
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types'
 import { PaymentSuccess } from '../../Illustrations/PaymentSuccess/index'
 import { PaymentError } from '../../Illustrations/PaymentError/index'
 import { ErrorLoading } from '../../Illustrations/ErrorLoading/index'
+import { NotFound } from '../../Illustrations/NotFound/index'
 
 const Illustration = ({ type, size }) => {
 	const illustrationProps = { size }
 	const illustrationList = {
 		paymentSuccess: <PaymentSuccess {...illustrationProps} />,
 		paymentError: <PaymentError {...illustrationProps} />,
-		errorLoading: <ErrorLoading {...illustrationProps} />
+		errorLoading: <ErrorLoading {...illustrationProps} />,
+		notFound: <NotFound {...illustrationProps} />
 	}
 	return illustrationList[type]
 }
