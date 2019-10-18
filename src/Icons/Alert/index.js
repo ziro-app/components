25154d3213206a1f71434d9b2c50ecd47ed63682
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-10-17 18:45:53 -0700

    minor updates to icons

--- src/Icons/Alert/index.js
+++ src/Icons/Alert/index.js
@@ -5,11 +5,11 @@ import { Scaffold } from '../Scaffold'
 export const Alert = ({ style, onClick, size, color, strokeWidth }) =>
 	<Scaffold
 		svgPath={
-			<Fragment>
+			<>
 				<circle cx='12' cy='12' r='10'></circle>
 				<line x1='12' y1='8' x2='12' y2='12'></line>
 				<line x1='12' y1='16' x2='12' y2='16'></line>
-			</Fragment>
+			</>
 		}
 		style={style}
 		onClick={onClick}
