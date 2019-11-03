8e2fc3ead9cb1c039ba3cfd2e118c3c584b99556
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-11-03 09:08:12 -0800

    add map via mapbox

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
 import Logo from '../Logo/index'
 import { container, name } from './styles'
 
-const About = () =>
+const About = () => {
+	useEffect(() => {
+		mapboxgl.accessToken = '';
+		var map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [-46.638279672833505,-23.529533748594474],
+			zoom: 15
+		});
+	}, [])
+	return (
 	<div style={container}>
 		<Logo />
 		<label style={name}>Ziro Negócios Digitais Ltda</label>
 		<label>28.026.371/0001-61</label>
 		<label>Blog | Catálogo</label>
 		<label>R. Lubavitch, 71, Bom Retiro - SP</label>
+		<div style={{ width: '100%', height: '400px' }} id='map'></div>
 	</div>
+	)
+}
 
 About.propTypes = {
 }
