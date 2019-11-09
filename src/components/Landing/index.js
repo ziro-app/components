9c25d16200a8aca47d06f5781ce4a359015005a9
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-11-08 16:58:31 -0800

    add first block in landing component

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Logo from '../Logo/index'
+import phones from './phones.png'
+import { heroCall, heroText, heroImg } from './styles'
 
 const Landing = () => {
 	return (
-		<div>
-			Hello
-		</div>
+		<>
+			<Logo size={40} />
+			<p style={heroCall}>Compre sem dificuldade roupas femininas para revender</p>
+			<p style={heroText}>A Ziro facilita na escolha, no despacho e no pagamento da sua mercadoria</p>
+			<img style={heroImg} src={phones} />
+		</>
+		
 	)
 }
 
---
+++ src/components/Landing/styles.js
@@ -0,0 +1,19 @@
+import { fontTitle, primaryColor } from '../../Theme/variables'
+
+export const
+
+heroCall = {
+	fontFamily: fontTitle,
+	fontSize: '3.2rem',
+	lineHeight: '1.2',
+	textAlign: 'left'
+},
+
+heroText = {
+	fontSize: '2.1rem',
+	textAlign: 'start'
+},
+
+heroImg = {
+	width: '100%'
+}
