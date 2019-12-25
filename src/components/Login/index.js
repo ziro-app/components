3d766ca481e4c455712532fce5cfd9278e9a82b8
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-24 16:12:55 -0800

    add Form to login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Form from '../Form/index'
 import FormInput from '../FormInput/index'
 import InputText from '../InputText/index'
 
 const Login = () => {
+	const [email, setEmail] = useState('')
 	return (
-		<FormInput label='Email' input={
-			<InputText />
-		} />
+		<Form
+			validations={[]}
+			sendToBackend={() => console.log('submit')}
+			inputs={[
+				<FormInput label='Email' input={
+					<InputText
+						value={email}
+						onChange={({ target: { value } }) => setEmail(value)}
+						placeholder='Seu email'
+					/>
+				}/>
+			]}
+		/>
 	)
 }
 
