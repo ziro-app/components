114a505b0976491d98b1a4c648e51325a74752fa
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-25 09:17:22 -0800

    add more components to LoginPage

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import HeaderHome from '../HeaderHome/index'
 import LoginForm from '../LoginForm/index'
+import { welcome, subtitle, dot } from './styles'
 
 const LoginPage = () => {
 	return (
-		<LoginForm />
+		<>
+			<HeaderHome linkPath='/cadastrar' linkText='Sem conta? CADASTRAR' />
+			<h1 style={welcome}>
+				<label style={subtitle}>Bem-vindo</label>
+				<label style={dot}>.</label>
+				<br/>Acesse sua conta,
+			</h1>
+			<LoginForm />
+		</>
 	)
 }
 
---
+++ src/components/LoginPage/styles.js
@@ -0,0 +1,18 @@
+import { fontTitle } from '../../Theme/variables'
+
+export const
+
+welcome = {
+	fontFamily: fontTitle,
+	fontSize: '2.1rem'
+},
+
+subtitle = {
+	fontSize: '1.4rem',
+	textTransform: 'uppercase'
+},
+
+dot = {
+	color: '#FFE401',
+	fontSize: '2.1rem'
+}
