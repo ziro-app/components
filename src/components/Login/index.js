11869685eafb439b037186e4c61001b5168910fe
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-24 16:29:55 -0800

    add error validation

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -5,16 +5,38 @@ import InputText from '../InputText/index'
 
 const Login = () => {
 	const [email, setEmail] = useState('')
+	const [pass, setPass] = useState('')
+	const validations = [
+		{
+			name: 'email',
+			validation: value => /^\S+@\S+\.\S+$/g.test(value), // tests for pattern a@b.c
+			value: email,
+			message: 'Formato inválido'
+		},
+		{
+			name: 'pass',
+			validation: value => !/^.{0,5}$/g.test(value), // tests for min length of 6 char
+			value: pass,
+			message: 'Mínimo 6 caracteres'
+		}
+	]
 	return (
 		<Form
-			validations={[]}
+			validations={validations}
 			sendToBackend={() => console.log('submit')}
 			inputs={[
-				<FormInput label='Email' input={
+				<FormInput name='email' label='Email' input={
 					<InputText
 						value={email}
 						onChange={({ target: { value } }) => setEmail(value)}
 						placeholder='Seu email'
+					/>
+				}/>,
+				<FormInput name='pass' label='Senha' input={
+					<InputText
+						value={pass}
+						onChange={({ target: { value } }) => setPass(value)}
+						placeholder='Sua senha'
 					/>
 				}/>
 			]}
