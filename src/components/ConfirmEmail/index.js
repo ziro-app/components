138499702ee7315151b464d0d479ce03964d7626
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-30 10:56:47 -0800

    add submission states

--- src/components/ConfirmEmail/index.js
+++ src/components/ConfirmEmail/index.js
@@ -5,8 +5,21 @@ import Button from '../Button/index'
 import { successColor } from '../../Theme/variables'
 import { container, custom, blockOne, blockTwo, btnWhite } from './styles'
 
-const ConfirmEmail = ({ email }) => {
+const ConfirmEmail = ({ email, auth }) => {
 	const [submitting, setSubmitting] = useState(false)
+	const [message, setMessage] = useState({ content: '', type: '' })
+	const onClick = async () => {
+		try {
+			setSubmitting(true)
+			await auth.currentUser.sendEmailVerification({ url: `${process.env.CONTINUE_URL}` })
+			setSubmitting(false)
+			setMessage({ content: 'Email reenviado com sucesso!', type: 'success' })
+		} catch (error) {
+			console.log(error)
+			setSubmitting(false)
+			setMessage({ content: 'Aguarde alguns instantes para reenviar', type: 'error' })
+		}
+	}
 	return (
 		<div style={container}>
 			<Badge
@@ -23,15 +36,16 @@ const ConfirmEmail = ({ email }) => {
 				<label>Apenas após confirmação o login será liberado</label>
 			</div>
 			<div style={blockTwo}>
-				<Button type='link' cta='Já acessei o link' />
-				<Button type='click' cta='Reenviar email' submitting={submitting} style={btnWhite} />
+				<Button type='link' cta='Já acessei o link' navigate={() => window.location.replace('/')} />
+				<Button type='click' cta='Reenviar email' click={onClick} submitting={submitting} style={btnWhite} />
 			</div>
 		</div>
 	)
 }
 
 ConfirmEmail.propTypes = {
-	email: Proptypes.string.isRequired
+	email: Proptypes.string.isRequired,
+	auth: Proptypes.object.isRequired
 }
 
 export default ConfirmEmail
