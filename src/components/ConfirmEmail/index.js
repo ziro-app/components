0d42cbe8cef1b739b3fd08cda888af310470e651
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-29 16:26:30 -0800

    start designing confirmEmail component

--- src/components/ConfirmEmail/index.js
+++ src/components/ConfirmEmail/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import Proptypes from 'prop-types'
+import Button from '../Button/index'
+import { container } from './styles'
 
-const ConfirmEmail = () => {
+const ConfirmEmail = ({ email }) => {
 	return (
-		<div>
-			Confirm email
+		<div style={container}>
+			<label>Email de confirmação enviado para:</label>
+			<label>{email}</label>
+			<label>Clique no link recebido para acessar o app</label>
+			<Button type='link' cta='Já acessei o link' />
+			<Button type='link' cta='Reenviar email' />
 		</div>
 	)
+}
+
+ConfirmEmail.propTypes = {
+	email: Proptypes.string.isRequired
 }
 
 export default ConfirmEmail
---
+++ src/components/ConfirmEmail/styles.js
@@ -0,0 +1,16 @@
+import { fontTitle, fontSizeInput, gradient, shadow, primaryColor, grayColor4 } from '../../Theme/variables'
+
+export const
+
+container = {
+	display: 'grid',
+	textAlign: 'center'
+},
+
+btnDisabled = {
+	cursor: 'initial',
+	color: primaryColor,
+	background: 'none',
+	backgroundColor: grayColor4,
+	boxShadow: 'none'
+}
