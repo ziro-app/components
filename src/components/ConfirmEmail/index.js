7c050c3aa673c606a3abff3f9047b0f4d3adab96
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-30 11:16:08 -0800

    major refactor to confirm-email to make it stateless

--- src/components/ConfirmEmail/index.js
+++ src/components/ConfirmEmail/index.js
@@ -1,51 +1,30 @@
-import React, { useState } from 'react'
-import Proptypes from 'prop-types'
+import React from 'react'
 import Badge from '../Badge/index'
 import Button from '../Button/index'
 import { successColor } from '../../Theme/variables'
 import { container, custom, blockOne, blockTwo, btnWhite } from './styles'
 
-const ConfirmEmail = ({ email, auth }) => {
-	const [submitting, setSubmitting] = useState(false)
-	const [message, setMessage] = useState({ content: '', type: '' })
-	const onClick = async () => {
-		try {
-			setSubmitting(true)
-			await auth.currentUser.sendEmailVerification({ url: `${process.env.CONTINUE_URL}` })
-			setSubmitting(false)
-			setMessage({ content: 'Email reenviado com sucesso!', type: 'success' })
-		} catch (error) {
-			console.log(error)
-			setSubmitting(false)
-			setMessage({ content: 'Aguarde alguns instantes para reenviar', type: 'error' })
-		}
-	}
-	return (
-		<div style={container}>
-			<Badge
-				type='success'
-				style={custom(18, successColor)}
-				size={21}
-				color={successColor}
-				message='Cadastro feito com sucesso!'
-			/>
-			<div style={blockOne}>
-				<label>Email de confirmação enviado para:</label>
-				<label><strong>{email}</strong></label>
-				<label>Clique no link recebido para acessar o app</label>
-				<label>Apenas após confirmação o login será liberado</label>
-			</div>
-			<div style={blockTwo}>
-				<Button type='link' cta='Já acessei o link' navigate={() => window.location.replace('/')} />
-				<Button type='click' cta='Reenviar email' click={onClick} submitting={submitting} style={btnWhite} />
-			</div>
+const ConfirmEmail = () =>
+	<div style={container}>
+		<Badge
+			type='success'
+			style={custom(18, successColor)}
+			size={21}
+			color={successColor}
+			message='Cadastro feito com sucesso!'
+		/>
+		<div style={blockOne}>
+			<label>Email de confirmação enviado</label>
+			<label>Clique no link recebido para liberar seu login</label>
 		</div>
-	)
-}
-
-ConfirmEmail.propTypes = {
-	email: Proptypes.string.isRequired,
-	auth: Proptypes.object.isRequired
-}
+		<div style={blockTwo}>
+			<Button type='link' cta='Link acessado, fazer login'
+				navigate={() => window.location.replace('/login')}
+			/>
+			<Button type='link' cta='Não recebi o email' style={btnWhite}
+				navigate={() => window.location.replace('/reenviar-email')}
+			/>
+		</div>
+	</div>
 
 export default ConfirmEmail
