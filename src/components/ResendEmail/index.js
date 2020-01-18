92683d8e090596110d349b970de6fbf89662bfae
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-01-17 16:09:32 -0800

    update component

--- src/components/ResendEmail/index.js
+++ src/components/ResendEmail/index.js
@@ -1,41 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import Form from '../Form/index'
-import FormInput from '../FormInput/index'
-import InputText from '../InputText/index'
+import LoginForm from '../LoginForm/index'
 import { title } from './styles'
 
-const ResendEmail = ({ sendToBackend }) => {
-	const [email, setEmail] = useState('')
-	const state = { email, setEmail }
-	const validations = [
-		{
-			name: 'email',
-			validation: value => /^\S+@\S+\.\S+$/g.test(value), // tests for pattern a@b.c
-			value: email,
-			message: 'Formato inválido'
-		}
-	]
-	return (
-		<>
-			<h1 style={title}>Reenviar confirmação</h1>
-			<Form
-				buttonOnTop={true}
-				validations={validations}
-				sendToBackend={sendToBackend ? sendToBackend(state) : () => null}
-				inputs={[
-					<FormInput name='email' label='Email' input={
-						<InputText
-							value={email}
-							onChange={({ target: { value } }) => setEmail(value)}
-							placeholder='Seu email'
-						/>
-					}/>
-				]}
-			/>
-		</>
-	)
-}
+const ResendEmail = ({ sendToBackend }) =>
+	<>
+		<h1 style={title}>Reenviar confirmação</h1>
+		<LoginForm sendToBackend={sendToBackend} />
+	</>
 
 ResendEmail.propTypes = {
 	sendToBackend: PropTypes.func.isRequired
