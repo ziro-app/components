acdf156b779bc8ed657b6ba9d7398f63bb1f508d
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-01-20 18:28:34 -0800

    use setLocation

--- src/components/ConfirmEmail/index.js
+++ src/components/ConfirmEmail/index.js
@@ -1,30 +1,35 @@
 import React from 'react'
+import { useLocation } from 'wouter'
 import Badge from '../Badge/index'
 import Button from '../Button/index'
 import { successColor } from '../../Theme/variables'
 import { container, custom, blockOne, blockTwo, btnWhite } from './styles'
 
-const ConfirmEmail = () =>
-	<div style={container}>
-		<Badge
-			type='success'
-			style={custom(18, successColor)}
-			size={21}
-			color={successColor}
-			message='Cadastro feito com sucesso!'
-		/>
-		<div style={blockOne}>
-			<label>Email de confirmação enviado</label>
-			<label>Clique no link recebido para liberar seu login</label>
-		</div>
-		<div style={blockTwo}>
-			<Button type='link' cta='Link acessado, fazer login'
-				navigate={() => window.location.assign('/login')}
+const ConfirmEmail = () => {
+	const [, setLocation] = useLocation()
+	return (
+		<div style={container}>
+			<Badge
+				type='success'
+				style={custom(18, successColor)}
+				size={21}
+				color={successColor}
+				message='Cadastro feito com sucesso!'
 			/>
-			<Button type='link' cta='Não recebi o email' style={btnWhite}
-				navigate={() => window.location.assign('/reenviar-email')}
-			/>
+			<div style={blockOne}>
+				<label>Email de confirmação enviado</label>
+				<label>Clique no link recebido para liberar seu login</label>
+			</div>
+			<div style={blockTwo}>
+				<Button type='link' cta='Link acessado, fazer login'
+					navigate={() => setLocation('/login')}
+				/>
+				<Button type='link' cta='Não recebi o email' style={btnWhite}
+					navigate={() => setLocation('/reenviar-email')}
+				/>
+			</div>
 		</div>
-	</div>
+	)
+}
 
 export default ConfirmEmail
--- src/components/LoginTrouble/index.js
+++ src/components/LoginTrouble/index.js
@@ -1,20 +1,25 @@
 import React from 'react'
+import { useLocation } from 'wouter'
 import Header from '../Header/index'
 import Button from '../Button/index'
 import { container, btnWhite } from './styles'
 
-const LoginTrouble = () =>
-	<>
-		<Header type='icon-link' icon='back' navigateTo='/' title='Reenviar confirmação' />
-		<div style={container}>
-			<label>Escolha a opção desejada</label>
-			<Button type='link' cta='Reenviar confirmação'
-				navigate={() => window.location.assign('/confirmar-email')}
-			/>
-			<Button type='link' cta='Resetar senha' style={btnWhite}
-				navigate={() => window.location.assign('/resetar-senha')}
-			/>
-		</div>
-	</>
+const LoginTrouble = () => {
+	const [, setLocation] = useLocation()
+	return (
+		<>
+			<Header type='icon-link' icon='back' navigateTo='/' title='Reenviar confirmação' />
+			<div style={container}>
+				<label>Escolha a opção desejada</label>
+				<Button type='link' cta='Reenviar confirmação'
+					navigate={() => setLocation('/confirmar-email')}
+				/>
+				<Button type='link' cta='Resetar senha' style={btnWhite}
+					navigate={() => setLocation('/resetar-senha')}
+				/>
+			</div>
+		</>
+	)
+}
 
 export default LoginTrouble
