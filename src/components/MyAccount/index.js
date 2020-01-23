3bee4c00f7c61fae03de699b6097c09281b5ebd6
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-01-22 19:54:13 -0800

    add styles to options

--- src/components/MyAccount/index.js
+++ src/components/MyAccount/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
-import { container } from './styles'
+import Icon from '../Icon/index'
+import { container, option, dot } from './styles'
 
 const MyAccount = () =>
 	<div style={container}>
-		<label>Trocar senha</label>
-		<label>Trocar email</label>
-		<label>Deletar conta</label>
+		<label style={option}>Trocar senha&nbsp;&nbsp;
+			<Icon type='lock' size={14} strokeWidth={3} />
+			<span style={dot}>.</span>
+		</label>
+		<label style={option}>Trocar email&nbsp;&nbsp;
+			<Icon type='email' size={14} strokeWidth={3} />
+			<span style={dot}>.</span>
+		</label>
+		<label style={option}>Deletar conta&nbsp;
+			<Icon type='close' size={14} strokeWidth={3} />
+			<span style={dot}>.</span>
+		</label>
 	</div>
 
 export default MyAccount
--- src/components/MyAccount/styles.js
+++ src/components/MyAccount/styles.js
@@ -1,9 +1,26 @@
-import { fontTitle, fontSizeSmall, primaryColor } from '../../Theme/variables'
+import { primaryColor, secondaryColor } from '../../Theme/variables'
 
 export const
 
 container = {
 	display: 'grid',
-	fontFamily: fontTitle,
-	textTransform: 'uppercase'
+	gridRowGap: '30px',
+	marginTop: '40px',
+	color: primaryColor,
+	fontWeight: '500'
+},
+
+option = {
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+	borderRadius: '8px',
+	padding: '8px 0',
+	backgroundColor: '#FDFDFD',
+	boxShadow: `rgba(34, 34, 34, 0.45) 0px 3px 16px -2px`
+},
+
+dot = {
+	fontSize: '21px',
+	color: secondaryColor
 }
