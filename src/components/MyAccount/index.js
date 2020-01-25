ae140ca03a7702946d3c7c69d4196d48ad3f218c
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-01-24 21:36:09 -0800

    update routes for MyAccount

--- src/components/MyAccount/index.js
+++ src/components/MyAccount/index.js
@@ -6,19 +6,19 @@ import { container, option, dot } from './styles'
 
 const MyAccount = () =>
 	<div style={container}>
-		<Link href='/update-pass'>
+		<Link href='/trocar-senha'>
 			<label style={option}>Trocar senha&nbsp;&nbsp;
 				<Icon type='lock' size={14} strokeWidth={3} />
 				<span style={dot}>.</span>
 			</label>
 		</Link>
-		<Link href='/update-email'>
+		<Link href='/trocar-email'>
 			<label style={option}>Trocar email&nbsp;&nbsp;
 				<Icon type='email' size={14} strokeWidth={3} />
 				<span style={dot}>.</span>
 			</label>
 		</Link>
-		<Link href='/delete-account'>
+		<Link href='/deletar-conta'>
 			<label style={option}>Deletar conta&nbsp;
 				<Icon type='close' size={14} strokeWidth={3} />
 				<span style={dot}>.</span>
