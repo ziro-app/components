116ce5fc2defca1388a7097f00716ff841d8fdab
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-01-26 18:13:49 -0800

    finish removing component

--- src/App/DisplayComponents/DisplayReauthenticate/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Reauthenticate from '../../../components/Reauthenticate/index'
-
-export const DisplayReauthenticate = () =>
-	<Reauthenticate sendToBackend={
-		() => async () => await new Promise((res, rej) => setTimeout(() => res('Enviado!'), 1500))
-	} />
