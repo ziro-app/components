07b2c50a5c06b3f94f088150421db43595c18a59
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-01-28 20:30:05 -0800

    improve styles for MyAccount

--- src/components/MyAccount/index.js
+++ src/components/MyAccount/index.js
@@ -2,24 +2,24 @@ import React from 'react'
 import { Link } from 'wouter'
 import Icon from '../Icon/index'
 import Footer from '../Footer/index'
-import { container, option, dot } from './styles'
+import { container, option, optionWhite, dot } from './styles'
 
 const MyAccount = () =>
 	<div style={container}>
 		<Link href='/trocar-senha'>
 			<label style={option}>Trocar senha&nbsp;&nbsp;
-				<Icon type='lock' size={14} strokeWidth={3} />
+				<Icon type='lock' size={14} strokeWidth={3} color='white' />
 				<span style={dot}>.</span>
 			</label>
 		</Link>
 		<Link href='/trocar-email'>
 			<label style={option}>Trocar email&nbsp;&nbsp;
-				<Icon type='email' size={14} strokeWidth={3} />
+				<Icon type='email' size={14} strokeWidth={3} color='white' />
 				<span style={dot}>.</span>
 			</label>
 		</Link>
 		<Link href='/deletar-conta'>
-			<label style={option}>Deletar conta&nbsp;
+			<label style={optionWhite}>Deletar conta&nbsp;
 				<Icon type='close' size={14} strokeWidth={3} />
 				<span style={dot}>.</span>
 			</label>
--- src/components/MyAccount/styles.js
+++ src/components/MyAccount/styles.js
@@ -1,4 +1,4 @@
-import { primaryColor, secondaryColor } from '@ziro/theme'
+import { fontTitle, fontSizeInput, primaryColor, secondaryColor, gradient, shadow } from '@ziro/theme'
 
 export const
 
@@ -7,17 +7,35 @@ container = {
 	gridRowGap: '30px',
 	marginTop: '40px',
 	color: primaryColor,
-	fontWeight: '500'
+	fontWeight: '500',
+	textAlign: 'center'
 },
 
-option = {
+optionWhite = {
 	display: 'flex',
 	alignItems: 'center',
 	justifyContent: 'center',
-	borderRadius: '8px',
-	padding: '8px 0',
-	backgroundColor: '#FDFDFD',
-	boxShadow: `rgba(34, 34, 34, 0.45) 0px 3px 16px -2px`
+	WebkitAppearance: 'none',
+	WebkitTapHighlightColor: 'rgba(0,0,0,0)',
+	MozAppearance: 'none',
+	outline: 'none',
+	cursor: 'pointer',
+	width: '100%',
+	padding: '6px 0',
+	borderRadius: '25px',
+	fontFamily: fontTitle,
+	fontSize: fontSizeInput,
+	color: primaryColor,
+	background: `linear-gradient(rgb(244, 244, 244) 10%, rgb(247, 247, 247) 30%, rgb(250, 250, 250) 60%, rgb(255, 255, 255))`,
+	boxShadow: `rgba(34, 34, 34, 0.45) 0px 2px 15px -4px,rgba(34, 34, 34, 0.25) 0px 2px 15px -4px`
+},
+
+option = {
+	...optionWhite,
+	color: 'white',
+	background: gradient,
+	boxShadow: shadow
+
 },
 
 dot = {
