d4fd341ed142377c209b05c8662ba787c724a872
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-01-29 18:10:08 -0800

    add SocialMedia component

---
+++ src/App/DisplayComponents/DisplaySocialMedia/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import SocialMedia from '../../../components/SocialMedia/index'
+import { containerWithPadding } from '@ziro/theme'
+
+export const DisplaySocialMedia = () =>
+	<div style={containerWithPadding}>
+		<SocialMedia />
+	</div>
---
+++ src/components/SocialMedia/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Icon from '../Icon/index'
+// import { container, name } from './styles'
+
+const SocialMedia = () =>
+	/*<div style={container}>*/
+	<div>
+		<Icon type='facebook' size={24} />
+		<Icon type='instagram' size={24} />
+		<Icon type='globe' size={24} />
+	</div>
+
+export default SocialMedia
