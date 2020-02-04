70052fb4746603162e2ace6de04d8d0f210e5284
Wermeson Rocha <wermeson.rocha@ledstartup.studio>
2020-02-04 13:50:15 -0300

    Google's FontLoader added

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    plugins: [
+        {
+            resolve: 'gatsby-plugin-web-font-loader',
+            options: {
+                google: {
+                    families: ['Rubik:500,600', 'Work Sans:300,400,500']
+                }
+            }
+        }
+    ]
+}
