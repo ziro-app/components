34d3d927a3a62d9d9f1b403c7ea9223bfce38b15
João L. S. Silva <joao.silva@easysoft.com.br>
2020-02-06 18:00:26 -0300

    Camera component initial implementation

---
+++ src/components/Camera/styles.js
@@ -0,0 +1,36 @@
+import { fontTitle, fontSizeSmall, primaryColor, secondaryColor, fontSizeInput } from '@ziro/theme'
+
+export const
+
+container = {
+	display: 'grid',
+	gridRowGap: '20px'
+},
+
+imageContainer = {
+	padding: '10px 0px',
+	width: '100%',
+},
+
+buttonContainer = {
+	display: 'flex',
+	justifyContent: 'space-between'
+},
+
+btnHalf = {
+	display: 'inline',
+	width: '45%',
+},
+
+inputHalf = {
+	display: 'inline',
+	width: '45%',
+	display: 'inline-flex',
+	justifyContent: 'center',
+},
+
+input = {
+	fontFamily: fontTitle,
+	fontSize: fontSizeInput,
+	color: '#FFF',
+}
