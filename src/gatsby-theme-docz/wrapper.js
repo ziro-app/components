a95cfb99f1856b691f2d46c68d2a21b00143ce1b
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-02-11 18:40:45 -0800

    add config files

---
+++ src/gatsby-theme-docz/wrapper.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const Wrapper = ({ children }) =>
+	<>
+	    <Helmet>
+	        <meta charSet="utf-8" />
+	        <title>Componentes</title>
+	        <link rel="icon"
+	            type="image/png"
+	            href="https://res.cloudinary.com/ziro/image/upload/v1561160634/logo-app_fwothv.png" />
+	    </Helmet>
+	    {children}
+	</>
+
+export default Wrapper
