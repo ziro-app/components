d1b911ab85e7f11d50d47ad61c76b7f14c102f56
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-02-11 19:21:31 -0800

    fix ugly styles from docz

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -5,7 +5,7 @@ const Wrapper = ({ children }) =>
 	<>
 	    <Helmet>
 	        <meta charSet="utf-8" />
-	        <title>Componentes</title>
+	        <title>Docz</title>
 	        <link rel="icon"
 	            type="image/png"
 	            href="https://res.cloudinary.com/ziro/image/upload/v1561160634/logo-app_fwothv.png" />
