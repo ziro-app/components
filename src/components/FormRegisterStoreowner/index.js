b9ca7c903f60a7dc17f9c344bded08655b71e79d
Wermeson Rocha <wermesonrocha2@gmail.com>
2020-02-17 12:48:11 -0300

    Test searchCnpj function

--- src/components/FormRegisterStoreowner/index.js
+++ src/components/FormRegisterStoreowner/index.js
@@ -70,7 +70,7 @@ const FormRegisterStoreowner = ({ isLoading, setIsLoading, sendToBackend, name: 
 			name: 'birth',
 			validation: value => /^(?:(?:31(\/)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/.test(value),
 			value: birth,
-			message: 'Formato inválido'
+			message: 'Data inválida'
 		},
 		{
 			name: 'razao',
@@ -128,7 +128,7 @@ const FormRegisterStoreowner = ({ isLoading, setIsLoading, sendToBackend, name: 
 	if (isError) return <Error />
 	return (
 		<>
-			<GetCnpj cnpj={cnpj} setState={setState} storeowners={storeowners} setCnpjValid={setCnpjValid} searchCnpj={() => searchCnpj} />
+			<GetCnpj cnpj={cnpj} setState={setState} storeowners={storeowners} setCnpjValid={setCnpjValid} searchCnpj={searchCnpj} />
 			<Form
 				validations={validations}
 				sendToBackend={sendToBackend ? sendToBackend(state) : () => null}
