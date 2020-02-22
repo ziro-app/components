bdb77b354a2b33d4b11f67ee17d9b9b0d77df471
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-02-21 16:58:13 -0800

    improve design for ImageUpload

--- src/components/ImageUpload/index.js
+++ src/components/ImageUpload/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { dropzone, button, input, styleTag } from './styles'
 
 const ImageUpload = () => {
 	return (
-		<div>ImageUpload</div>
+		<div style={dropzone} className='dropzone'>
+			<style>{styleTag}</style>
+			<label>Arraste imagens ou escolha do dispositivo</label>
+			<label for='input-file' style={button}>Escolher</label>
+			<input
+				style={input}
+				type='file'
+				multiple={true}
+				id='input-file'
+			/>
+		</div>
 	)
 }
 
---
+++ src/components/ImageUpload/styles.js
@@ -0,0 +1,43 @@
+import { fontTitle, fontSizeInput, gradient, shadow, primaryColor, grayColor3 } from '@ziro/theme'
+
+export const
+
+dropzone = {
+	display: 'grid',
+	alignContent: 'center',
+	justifyItems: 'center',
+	border: `2px dashed ${grayColor3}`,
+	borderRadius: '6px',
+	color: primaryColor,
+	textAlign: 'center'
+},
+
+button = {
+	display: 'block', // necessary for link version
+	WebkitAppearance: 'none',
+	WebkitTapHighlightColor: 'rgba(0,0,0,0)',
+	MozAppearance: 'none',
+	outline: 'none',
+	cursor: 'pointer',
+	width: '100%',
+	padding: '10px 0px',
+	border: 'none',
+	borderRadius: '20px',
+	fontFamily: fontTitle,
+	fontSize: fontSizeInput,
+	color: '#FFF',
+	background: gradient,
+	boxShadow: `${shadow}`
+},
+
+input = {
+	opacity: 0
+},
+
+styleTag = `
+	.dropzone {
+		height: 100%;
+		height: -webkit-fill-available;
+		height: -moz-available;
+	}
+`
