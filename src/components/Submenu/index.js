a7a18c4d5c2953ce59c9721982887c9840fcf19d
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-02-23 16:51:29 -0800

    receive path and name

--- src/components/Submenu/index.js
+++ src/components/Submenu/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Link } from 'wouter'
 import PropTypes from 'prop-types'
+import { Link } from 'wouter'
 import { container, instructions, optionStyle } from './styles'
 
 const Submenu = ({ options }) =>
 	<div style={container}>
 		<label style={instructions}>Escolha a ferramenta desejada</label>
-		{options.map(option =>
-			<Link><a style={optionStyle}>{option}</a></Link>
+		{options.map(([ name, path], index) =>
+			<Link href={path} key={index}>
+				<a style={optionStyle}>{name}</a>
+			</Link>
 		)}
 	</div>
 
