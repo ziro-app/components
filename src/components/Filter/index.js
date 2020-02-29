dd5546ecb8fb2d45843e729320edf052af2275e0
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-02-29 15:13:58 -0800

    add svg radio buttons

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 // import { motion } from 'framer-motion'
 // import Button from '../Button/index'
 // import Icon from '../Icon/index'
-import { container, filter, title, description, filterTag, option, name, radio, apply } from './styles'
+import { container, filter, title, description, filterTag, option, name, nameSelected, radio, radioSelected, apply } from './styles'
 // import { animate, transition } from './animation'
 
 const Filter = () => {
@@ -16,16 +16,28 @@ const Filter = () => {
 				<label style={description}>Que vendem tendências:</label>
 				<div style={filterTag}>
 					<div style={option}>
-						<label style={name} htmlFor='animal print'>Animal Print</label>
-						<input style={radio} onChange={handleRadio} checked={tag === 'animal print'} type='radio' name='trend' value='animal print' id='animal print' />
+						<label style={tag === 'animal print' ? nameSelected : name} htmlFor='animal print'>Animal Print</label>
+						<input style={{ display: 'none' }} onChange={handleRadio} checked={tag === 'animal print'} type='radio' name='trend' value='animal print' id='animal print' />
+						<svg height='18' width='18' viewBox='0 0 18 18'>
+							<circle cx='9' cy='9' r='7' fill='none' stroke='black' strokeWidth={tag === 'animal print' ? '3' : '1'} />
+							{tag === 'animal print' ? <circle cx='9' cy='9' r='3' fill='black' /> : null}
+						</svg>
 					</div>
 					<div style={option}>
-						<label style={name} htmlFor='tweed'>Tweed</label>
-						<input style={radio} onChange={handleRadio} checked={tag === 'tweed'} type='radio' name='trend' value='tweed' id='tweed' />
+						<label style={tag === 'tweed' ? nameSelected : name} htmlFor='tweed'>Tweed</label>
+						<input style={{ display: 'none' }} onChange={handleRadio} checked={tag === 'tweed'} type='radio' name='trend' value='tweed' id='tweed' />
+						<svg height='18' width='18' viewBox='0 0 18 18'>
+							<circle cx='9' cy='9' r='7' fill='none' stroke='black' strokeWidth={tag === 'tweed' ? '3' : '1'} />
+							{tag === 'tweed' ? <circle cx='9' cy='9' r='3' fill='black' /> : null}
+						</svg>
 					</div>
 					<div style={option}>
-						<label style={name} htmlFor='retilinea'>Retilínea</label>
-						<input style={radio} onChange={handleRadio} checked={tag === 'retilinea'} type='radio' name='trend' value='retilinea' id='retilinea' />
+						<label style={tag === 'retilinea' ? nameSelected : name} htmlFor='retilinea'>Retilínea</label>
+						<input style={{ display: 'none' }} onChange={handleRadio} checked={tag === 'retilinea'} type='radio' name='trend' value='retilinea' id='retilinea' />
+						<svg height='18' width='18' viewBox='0 0 18 18'>
+							<circle cx='9' cy='9' r='7' fill='none' stroke='black' strokeWidth={tag === 'retilinea' ? '3' : '1'} />
+							{tag === 'retilinea' ? <circle cx='9' cy='9' r='3' fill='black' /> : null}
+						</svg>
 					</div>
 				</div>
 			</div>
--- src/components/Filter/styles.js
+++ src/components/Filter/styles.js
@@ -10,11 +10,12 @@ container = {
 	padding: '20px 20px 60px',
 	background: 'white',
 	display: 'grid',
-	alignContent: 'space-between'
+	alignContent: 'start'
 },
 
 filter = {
-
+	display: 'grid',
+	gridRowGap: '15px'
 },
 
 title = {
@@ -23,24 +24,48 @@ title = {
 },
 
 description = {
+	marginBottom: '5px',
 	fontFamily: fontTitle
 },
 
 filterTag = {
-
+	display: 'grid',
+	gridRowGap: '4px'
 },
 
 option = {
 	display: 'grid',
-	gridTemplateColumns: '1fr auto'
+	gridTemplateColumns: '1fr auto',
+	alignItems: 'center'
 },
 
 name = {
+	fontWeight: 300
+},
 
+nameSelected = {
+	...name,
+	fontWeight: 500
 },
 
 radio = {
+	WebkitAppearance: 'none',
+	MozAppearance: 'none',
+	MsAppearance: 'none',
+	OAppearance: 'none',
+	appearance: 'none',
+	WebkitTapHighlightColor: 'rgba(0,0,0,0)',
+	outline: 'none',
+	width: '15px',
+	height: '15px',
+	border: `1px solid ${primaryColor}`,
+	borderRadius: '50%'
+},
 
+radioSelected = {
+	...radio,
+	background: primaryColor,
+	boxShadow: shadow
 },
 
 apply = {
