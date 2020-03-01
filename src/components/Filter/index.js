716a8c2c78b7db5bc42836464479f4faebfb670f
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-02-29 18:35:43 -0800

    refactor radio buttons code

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -7,37 +7,26 @@ import { container, filter, description, filterTag, option, name, nameSelected, 
 // import { animate, transition } from './animation'
 
 const Filter = () => {
+	const trends = ['animal print', 'tweed', 'retilinea']
 	const [tag, setTag] = useState('')
 	const handleRadio = ({ target: { value } }) => setTag(value)
 	return (
 		<div style={container}>
 			<div style={filter}>
-				<label style={description}>Que vendem tendências:</label>
+				<label style={description}>Que vendem as tendências:</label>
 				<div style={filterTag}>
-					<div style={option}>
-						<label style={tag === 'animal print' ? nameSelected : name} htmlFor='animal print'>Animal Print</label>
-						<input style={{ display: 'none' }} onChange={handleRadio} checked={tag === 'animal print'} type='radio' name='trend' value='animal print' id='animal print' />
-						<svg height='18' width='18' viewBox='0 0 18 18'>
-							<circle cx='9' cy='9' r='7' fill='none' stroke='black' strokeWidth={tag === 'animal print' ? '3' : '1'} />
-							{tag === 'animal print' ? <circle cx='9' cy='9' r='3' fill='black' /> : null}
-						</svg>
-					</div>
-					<div style={option}>
-						<label style={tag === 'tweed' ? nameSelected : name} htmlFor='tweed'>Tweed</label>
-						<input style={{ display: 'none' }} onChange={handleRadio} checked={tag === 'tweed'} type='radio' name='trend' value='tweed' id='tweed' />
-						<svg height='18' width='18' viewBox='0 0 18 18'>
-							<circle cx='9' cy='9' r='7' fill='none' stroke='black' strokeWidth={tag === 'tweed' ? '3' : '1'} />
-							{tag === 'tweed' ? <circle cx='9' cy='9' r='3' fill='black' /> : null}
-						</svg>
-					</div>
-					<div style={option}>
-						<label style={tag === 'retilinea' ? nameSelected : name} htmlFor='retilinea'>Retilínea</label>
-						<input style={{ display: 'none' }} onChange={handleRadio} checked={tag === 'retilinea'} type='radio' name='trend' value='retilinea' id='retilinea' />
-						<svg height='18' width='18' viewBox='0 0 18 18'>
-							<circle cx='9' cy='9' r='7' fill='none' stroke='black' strokeWidth={tag === 'retilinea' ? '3' : '1'} />
-							{tag === 'retilinea' ? <circle cx='9' cy='9' r='3' fill='black' /> : null}
-						</svg>
-					</div>
+					{trends.map(trend => {
+						return (
+							<div style={option}>
+								<label style={tag === trend ? nameSelected : name} htmlFor={trend}>{trend}</label>
+								<input style={{ display: 'none' }} onChange={handleRadio} checked={tag === trend} type='radio' name='trend' value={trend} id={trend} />
+								<svg height='18' width='18' viewBox='0 0 18 18'>
+									<circle cx='9' cy='9' r='7' fill='none' stroke='black' strokeWidth={tag === trend ? '3' : '1'} />
+									{tag === trend ? <circle cx='9' cy='9' r='3' fill='black' /> : null}
+								</svg>
+							</div>
+						)
+					})}
 				</div>
 			</div>
 			<div style={apply}>
