f0d56d059d0384b6f60134a91a8d25cb41032e22
João Lucas Silva <joao.lucas.ss@hotmail.com>
2020-03-16 18:09:15 -0300

    add more animations to flow manager

--- src/components/FlowManager/defaultAnimations.js
+++ src/components/FlowManager/defaultAnimations.js
@@ -10,4 +10,16 @@ goRight = {
     exit: { x: '20%', opacity: 0 },
     enter: { x: '0%', opacity: 1 },
     initial: { x: '-20%', opacity: 0 }
+},
+
+goUp = {
+    exit: { y: '-10%', opacity: 0 },
+    enter: { y: '0%', opacity: 1 },
+    initial: { y: '10%', opacity: 0 }
+},
+
+goDown = {
+    exit: { y: '10%', opacity: 0 },
+    enter: { y: '0%', opacity: 1 },
+    initial: { y: '-10%', opacity: 0 }
 }
