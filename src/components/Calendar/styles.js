f5a9210871477870b4790b4a762741b6eea25edf
Wermeson Rocha <wermesonrocha2@gmail.com>
2020-03-17 22:32:13 -0300

    Small css adjustments

--- src/components/Calendar/styles.js
+++ src/components/Calendar/styles.js
@@ -1,34 +1,65 @@
 import { fontBody, fontSizeInput, primaryColor, grayColor1, grayColor3 } from '@ziro/theme'
 
 export const styleTag = `
-	.SingleDatePickerInput__showClearDate_3 {
+    .DateInput {
+        margin: 0;
+        padding: 0;
+        background: #fff;
+        position: relative;
+        display: inline-block;
+        width: 80%;
+        vertical-align: middle;
+    }
+
+    .SingleDatePicker_1 {
         WebkitAppearance: none;
         MozAppearance: none;
         outline: none;
         box-sizing: border-box;
         width: 100%;
+        border-radius: 8px;
+        font-family: ${fontBody}, system-ui, sans-serif;
+        font-size: ${fontSizeInput};
+        color: ${primaryColor};
+        background-color: '#FDFDFD';
         border: 2px solid ${grayColor3};
+        box-shadow: rgba(34,34,34,0.3) 0px 3px 10px -3px;
+    }
+
+    .SingleDatePickerInput__showClearDate {
+        border: none;
+        padding-right: 0;
+    }
+`,
+ styleTagFocus = `
+    .DateInput {
+        margin: 0;
+        padding: 0;
+        background: #fff;
+        position: relative;
+        display: inline-block;
+        width: 80%;
+        vertical-align: middle;
+    }
+
+    .SingleDatePicker_1 {
+        WebkitAppearance: none;
+        MozAppearance: none;
+        outline: none;
+        box-sizing: border-box;
+        width: 100%;
         border-radius: 8px;
         font-family: ${fontBody}, system-ui, sans-serif;
         font-size: ${fontSizeInput};
         color: ${primaryColor};
         background-color: '#FDFDFD';
         box-shadow: rgba(34,34,34,0.3) 0px 3px 10px -3px;
-    }
-`, 
-styleTagFocus = `
-    .SingleDatePickerInput__showClearDate_3 {
-        WebkitAppearance: none;
-        MozAppearance: none;
-        outline: none;
-        box-sizing: border-box;
-        width: 100%;
         border: 2px solid ${grayColor1} !important;
-        border-radius: 8px;
-        font-family: ${fontBody}, system-ui, sans-serif;
-        font-size: ${fontSizeInput};
-        color: ${primaryColor};
-        background-color: '#FDFDFD';
         box-shadow: rgba(34, 34, 34, 0.3) 0px 3px 10px -2px !important;
     }
-`
+
+    .SingleDatePickerInput__showClearDate {
+        border: none;
+        padding-right: 0;
+    }
+ `
