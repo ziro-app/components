4e4789949aadd2ac7a42c15ec64786ba315dddf9
Alessandro <vyseclown@gmail.com>
2020-04-17 18:16:23 -0300

    Adding illustration withoutFavorites

--- src/components/Illustration/index.js
+++ src/components/Illustration/index.js
@@ -1,32 +1,34 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { PaymentSuccess } from '../../Illustrations/PaymentSuccess/index'
-import { PaymentError } from '../../Illustrations/PaymentError/index'
-import { ErrorLoading } from '../../Illustrations/ErrorLoading/index'
-import { NotFound } from '../../Illustrations/NotFound/index'
-import { TimelineStart } from '../../Illustrations/TimelineStart/index'
-import { ProfileData } from '../../Illustrations/ProfileData/index'
-import { Buy } from '../../Illustrations/Buy'
-import { RegisterSuccess } from '../../Illustrations/RegisterSuccess'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { PaymentSuccess } from '../../Illustrations/PaymentSuccess/index';
+import { PaymentError } from '../../Illustrations/PaymentError/index';
+import { ErrorLoading } from '../../Illustrations/ErrorLoading/index';
+import { NotFound } from '../../Illustrations/NotFound/index';
+import { TimelineStart } from '../../Illustrations/TimelineStart/index';
+import { ProfileData } from '../../Illustrations/ProfileData/index';
+import { Buy } from '../../Illustrations/Buy';
+import { RegisterSuccess } from '../../Illustrations/RegisterSuccess';
+import { WithoutFavorites } from '../../Illustrations/WithoutFavorites';
 
 const Illustration = ({ type, size }) => {
-	const illustrationProps = { size }
-	const illustrationList = {
-		paymentSuccess: <PaymentSuccess {...illustrationProps} />,
-		paymentError: <PaymentError {...illustrationProps} />,
-		errorLoading: <ErrorLoading {...illustrationProps} />,
-		notFound: <NotFound {...illustrationProps} />,
-		timelineStart: <TimelineStart {...illustrationProps} />,
-		profileData: <ProfileData {...illustrationProps} />,
-		buy: <Buy {...illustrationProps} />,
-		registerSuccess: <RegisterSuccess {...illustrationProps} />
-	}
-	return illustrationList[type]
-}
+  const illustrationProps = { size };
+  const illustrationList = {
+    paymentSuccess: <PaymentSuccess {...illustrationProps} />,
+    paymentError: <PaymentError {...illustrationProps} />,
+    errorLoading: <ErrorLoading {...illustrationProps} />,
+    notFound: <NotFound {...illustrationProps} />,
+    timelineStart: <TimelineStart {...illustrationProps} />,
+    profileData: <ProfileData {...illustrationProps} />,
+    buy: <Buy {...illustrationProps} />,
+    registerSuccess: <RegisterSuccess {...illustrationProps} />,
+    withoutFavorites: <WithoutFavorites {...illustrationProps} />,
+  };
+  return illustrationList[type];
+};
 
 Illustration.propTypes = {
-	type: PropTypes.string.isRequired,
-	size: PropTypes.number
-}
+  type: PropTypes.string.isRequired,
+  size: PropTypes.number,
+};
 
-export default Illustration
+export default Illustration;
