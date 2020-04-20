6f99df14733f52275db8cbf358d7ed8e5b4af0d3
João L. S. Silva <joao.silva@easysoft.com.br>
2020-04-20 09:33:46 -0300

    update flow-register-card

--- src/components/HeaderFlow/index.js
+++ src/components/HeaderFlow/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { fontTitle } from '@ziro/theme'
 
 const HeaderFlow = ({ title }) => {
 
     return (
         <div style={{ display: 'grid', height: '50px', alignItems: 'center' }}>
-            <label style={{ textAlign: 'center', fontSize: '1.8rem' }}>{title}</label>
+            <label style={{ textAlign: 'center', fontSize: '1.8rem', fontFamily: fontTitle }}>{title}</label>
         </div>
     )
 }
