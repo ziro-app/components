4b22dac5ebcc2f49d0ca75563583bb28f9c366fb
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-04-21 15:08:47 -0700

    style MenuHover

--- src/components/MenuHover/styles.js
+++ src/components/MenuHover/styles.js
@@ -1,5 +1,26 @@
+import { fontTitle, fontSizeSmall } from '@ziro/theme'
+
 export const
 
-container = {},
-menuOptions = {},
-option = {}
+container = {
+	display: 'grid',
+	alignItems: 'center',
+	justifyItems: 'center',
+	width: '70%',
+	height: '32px',
+	margin: '0 auto',
+	borderRadius: '25px',
+	boxShadow: `1px 0px 8px 0px rgba(34,34,34,0.15), 1px 0px 8px 0px rgba(34,34,34,0.10),
+	1px 0px 8px 0px rgba(34,34,34,0.05)`
+},
+
+menuOptions = {
+	display: 'flex',
+	justifyContent: 'space-between',
+	width: '75%'
+},
+
+option = {
+	fontFamily: fontTitle,
+	fontSize: '1.4rem'
+}
