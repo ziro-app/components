c4443150d5da30b0ebdfe9618e7168d0d2aad884
Alessandro <vyseclown@gmail.com>
2020-04-21 16:42:15 -0300

    Adding a bottomTab by index

---
+++ src/components/BottomTabBarByIndex/index.js
@@ -0,0 +1,60 @@
+import React, { useMemo, useCallback } from 'react';
+import { useAnimatedLocation } from '../FlowManager';
+import { container, button, notification } from './styles';
+
+const BottomTabBar = ({ buttons = [] }) => {
+  const [currentLocation, setLocation] = useAnimatedLocation();
+  const [currentIndex, gridTemplateColumns] = useMemo(
+    () => [
+      buttons.findIndex(
+        ({ location, secondaryLocations = [] }) =>
+          currentLocation.startsWith(location) ||
+          secondaryLocations.some((loc) =>
+            currentLocation.startsWith(loc)
+          )
+      ),
+      buttons.map(() => '1fr').join(' '),
+    ],
+    [currentLocation, buttons]
+  );
+
+  return (
+    <div style={{ ...container, gridTemplateColumns }}>
+      {buttons.map(
+        ({ icon, location, notificationNumber }, index) => {
+          const isSelected = useMemo(() => currentIndex === index, [
+            currentIndex,
+          ]);
+          const animation = useMemo(
+            () => (currentIndex > index ? 'goRight' : 'goLeft'),
+            [currentIndex]
+          );
+          const onClick = useCallback(
+            () =>
+              isSelected
+                ? currentLocation === location
+                  ? window.scrollTo({
+                      top: 0,
+                      left: 0,
+                      behavior: 'smooth',
+                    })
+                  : setLocation('goRight', location)
+                : setLocation(animation, location),
+            [isSelected, currentLocation]
+          );
+
+          return (
+            <div key={index} style={button} onClick={onClick}>
+              {icon(isSelected)}
+              {notificationNumber && notificationNumber > 0 ? (
+                <div style={notification}>{notificationNumber}</div>
+              ) : null}
+            </div>
+          );
+        }
+      )}
+    </div>
+  );
+};
+
+export default BottomTabBar;
---
+++ src/components/BottomTabBarByIndex/styles.js
@@ -0,0 +1,30 @@
+import { secondaryColor } from '@ziro/theme'
+
+export const
+
+container = {
+    display: 'grid',
+    padding: '10px'
+},
+
+button = {
+    height: '40px',
+    display: 'grid',
+    alignItems: 'center',
+    justifyItems: 'center',
+    position: 'relative'
+},
+
+notification = {
+    width: 15,
+    height: 15,
+    background: secondaryColor,
+    position: 'absolute',
+    top: '20%',
+    right: '30%',
+    borderRadius: '50%',
+    display: 'grid',
+    alignItems: 'center',
+    justifyContent: 'center',
+    boxShadow: '1px 1px 2px rgb(0,0,0,0.3) '
+}
