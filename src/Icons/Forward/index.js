805ae0f806149218305e86175484ebd3bb744d68
Alessandro <vyseclown@gmail.com>
2020-05-07 18:39:38 -0300

    Fixing some dom errors.

--- src/Icons/Forward/index.js
+++ src/Icons/Forward/index.js
@@ -18,7 +18,7 @@ export const Forward = ({
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
-    class="feather feather-arrow-right"
+    className="feather feather-arrow-right"
   >
     <line x1="5" y1="12" x2="19" y2="12"></line>
     <polyline points="12 5 19 12 12 19"></polyline>
