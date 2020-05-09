dff30f978212657501f3342963043d1a11737411
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-05-09 12:26:02 -0700

    add card views

--- src/App/DisplayComponents/DisplayCard/index.js
+++ src/App/DisplayComponents/DisplayCard/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '../../../components/Card/index';
+import { grid, title } from './styles';
 import { container } from '@ziro/theme';
 
 export const DisplayCard = () => {
@@ -12,21 +13,21 @@ export const DisplayCard = () => {
     price: '14990',
     pricetag: 'Não',
     productId: 'hKxUGL9CuKScXvBQ98Ve',
-    status: 'soldOut',
+    status: 'available',
     timeInDays: 2,
     url:
       'https://firebasestorage.googleapis.com/v0/b/ziro-app-data.appspot.com/o/Salgunamu%2FSalgunamu-1588112070123-VESTIDO%20COM%20FORRO%20149%2C90.jpg?alt=media&token=50ed388c-ab3f-4a82-aaad-18a2933a5d3c',
   };
   const photo2 = {
     brandName: 'Salgunamu',
-    description: 'Vestido forro',
+    description: '',
     favQuantity: 0,
     photoPeriod: 'Nova',
     cartQuantity: 2,
     price: '14990',
     pricetag: 'Não',
     productId: 'hKxUGL9CuKScXvBQ98Ve',
-    status: 'soldOut',
+    status: 'available',
     timeInDays: 2,
     url:
       'https://firebasestorage.googleapis.com/v0/b/ziro-app-data.appspot.com/o/Salgunamu%2FSalgunamu-1588112070123-VESTIDO%20COM%20FORRO%20149%2C90.jpg?alt=media&token=50ed388c-ab3f-4a82-aaad-18a2933a5d3c',
@@ -40,18 +41,87 @@ export const DisplayCard = () => {
     price: '14990',
     pricetag: 'Não',
     productId: 'hKxUGL9CuKScXvBQ98Ve',
+    status: 'available',
+    timeInDays: 2,
+    url:
+      'https://firebasestorage.googleapis.com/v0/b/ziro-app-data.appspot.com/o/Salgunamu%2FSalgunamu-1588112070123-VESTIDO%20COM%20FORRO%20149%2C90.jpg?alt=media&token=50ed388c-ab3f-4a82-aaad-18a2933a5d3c',
+  };
+  const photo4 = {
+    brandName: 'Salgunamu',
+    description: 'Vestido forro',
+    favQuantity: 0,
+    photoPeriod: 'Nova',
+    cartQuantity: 2,
+    price: '14990',
+    pricetag: 'Não',
+    productId: 'hKxUGL9CuKScXvBQ98Ve',
     status: 'soldOut',
     timeInDays: 2,
     url:
       'https://firebasestorage.googleapis.com/v0/b/ziro-app-data.appspot.com/o/Salgunamu%2FSalgunamu-1588112070123-VESTIDO%20COM%20FORRO%20149%2C90.jpg?alt=media&token=50ed388c-ab3f-4a82-aaad-18a2933a5d3c',
   };
-  const favoriteIds = ['hKxUGL9CuKScXvBQ98Ve'];
-  const cartIds = ['hKxUGL9CuKScXvBQ98Ve'];
+  const photo5 = {
+    brandName: 'Salgunamu',
+    description: '',
+    favQuantity: 0,
+    photoPeriod: 'Nova',
+    cartQuantity: 2,
+    price: '',
+    pricetag: 'Não',
+    productId: 'hKxUGL9CuKScXvBQ98Ve',
+    status: 'available',
+    timeInDays: 2,
+    url:
+      'https://firebasestorage.googleapis.com/v0/b/ziro-app-data.appspot.com/o/Salgunamu%2FSalgunamu-1588112070123-VESTIDO%20COM%20FORRO%20149%2C90.jpg?alt=media&token=50ed388c-ab3f-4a82-aaad-18a2933a5d3c',
+  };
+  const photo6 = {
+    brandName: 'Salgunamu',
+    description: '',
+    favQuantity: 0,
+    photoPeriod: 'Nova',
+    cartQuantity: 0,
+    price: '',
+    pricetag: 'Não',
+    productId: 'hKxUGL9CuKScXvBQ98Ve',
+    status: 'available',
+    timeInDays: 1,
+    url:
+      'https://firebasestorage.googleapis.com/v0/b/ziro-app-data.appspot.com/o/Salgunamu%2FSalgunamu-1588112070123-VESTIDO%20COM%20FORRO%20149%2C90.jpg?alt=media&token=50ed388c-ab3f-4a82-aaad-18a2933a5d3c',
+  };
+  const photo7 = {
+    brandName: 'Salgunamu',
+    description: '',
+    favQuantity: 0,
+    photoPeriod: 'Nova',
+    cartQuantity: 0,
+    price: '',
+    pricetag: 'Não',
+    productId: 'hKxUGL9CuKScXvBQ98Ve',
+    status: 'available',
+    timeInDays: 0,
+    url:
+      'https://firebasestorage.googleapis.com/v0/b/ziro-app-data.appspot.com/o/Salgunamu%2FSalgunamu-1588112070123-VESTIDO%20COM%20FORRO%20149%2C90.jpg?alt=media&token=50ed388c-ab3f-4a82-aaad-18a2933a5d3c',
+  };
+  const favoriteIds = ['ahKxUGL9CuKScXvBQ98Ve'];
+  const favoriteIdsSelected = ['hKxUGL9CuKScXvBQ98Ve'];
+  const cartIds = ['ahKxUGL9CuKScXvBQ98Ve'];
+  const cartIdsSelected = ['hKxUGL9CuKScXvBQ98Ve'];
   return (
-    <div style={container}>
+    <div style={{...container, maxWidth: '400px', display: 'grid', gridRowGap: '30px', margin: '30px auto'}}>
+      <label style={title}>Deslogado</label>
       <Card favoriteIds={favoriteIds} cartIds={cartIds} photo={photo1} uid={false} />
-      <Card favoriteIds={favoriteIds} cartIds={cartIds} photo={photo2} uid={true} />
-      <Card favoriteIds={favoriteIds} cartIds={cartIds} photo={photo3} uid={true} />
+      <label style={title}>Deslogado e sem descrição</label>
+      <Card favoriteIds={favoriteIds} cartIds={cartIds} photo={photo2} uid={false} />
+      <label style={title}>Logado</label>
+      <Card favoriteIds={favoriteIdsSelected} cartIds={cartIdsSelected} photo={photo3} uid={true} />
+      <label style={title}>Logado e Esgotado</label>
+      <Card favoriteIds={favoriteIds} cartIds={cartIds} photo={photo4} uid={true} />
+      <label style={title}>Logado, sem descrição e sem preço</label>
+      <Card favoriteIds={favoriteIds} cartIds={cartIds} photo={photo5} uid={true} />
+      <label style={title}>Logado, versão mínima</label>
+      <Card favoriteIds={favoriteIds} cartIds={cartIds} photo={photo6} uid={true} />
+      <label style={title}>Deslogado, versão mínima</label>
+      <Card favoriteIds={favoriteIds} cartIds={cartIds} photo={photo7} uid={false} />
     </div>
   );
 };
--- src/App/DisplayComponents/DisplayCard/styles.js
+++ src/App/DisplayComponents/DisplayCard/styles.js
@@ -1,9 +1,15 @@
+import { fontTitle } from '@ziro/theme'
+
 export const
 
 grid = {
 	display: 'grid',
-	gridTemplateColumns: '1fr 1fr',
 	gridRowGap: '20px',
 	alignContent: 'start',
 	justifyItems: 'center'
+},
+
+title = {
+	fontFamily: fontTitle,
+	fontSize: '2.1rem'
 }
