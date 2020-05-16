f5e6f15a468f83e23eb211ee9cb6cb5a7562f9ec
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-05-16 16:13:56 -0700

    minor fix

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,13 +4,13 @@ import { container, help } from './styles'
 
 const Footer = ({ phone }) =>
 	<div style={container}>
-		<label style={help} onClick={() => window.open('https://api.whatsapp.com/send?phone=551133340920', '_blank')}>
-			Ajuda? Whats: {phone}
+		<label style={help} onClick={() => window.open(`https://api.whatsapp.com/send?phone=${phone.replace(/\+|\s|\(|\)|-/g,'')}`, '_blank')}>
+			Ajuda? Whats: {phone.substring(4)}
 		</label>
 	</div>
 
 Footer.propTypes = {
-	phone: PropTypes.string
+	phone: PropTypes.string.isRequired
 }
 
 export default Footer
