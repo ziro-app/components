ad97db899e3220d82eea83f6928045566f15cea3
Wermeson Rocha <wermesonrocha2@gmail.com>
2020-05-26 16:39:46 -0300

    Corrections in the cnpj validation component

--- src/components/GetCnpj/searchCnpj.js
+++ src/components/GetCnpj/searchCnpj.js
@@ -1,5 +1,32 @@
 import axios from 'axios';
 
+const lastReq = async (config, validCnaes) => {
+    let result = {};
+    try {
+        const { data: { status, result } } = await axios(config);
+        if (status) {
+            const cnaes = [...result.atividades_secundarias, result.atividade_principal].map(({ code }) => code)
+            const cnaeIsValid = !!cnaes.filter(code => validCnaes.includes(code)).pop()
+            if (!cnaeIsValid) throw { msg: 'CNPJ não tem CNAE válido', customError: true }
+            const isActive = result.situacao === 'ATIVA'
+            if (!isActive) throw { msg: 'CNPJ não está ativo', customError: true }
+            result['reason'] = result.nome;
+            result['fantasia'] = result.fantasia;
+            result['street'] = result.logradouro;
+            result['number'] = result.numero;
+            result['complement'] = result.complemento;
+            result['neighborhood'] = result.bairro;
+            result['cep'] = result.cep.replace('.', '');
+            result['city'] = result.municipio;
+            result['cityState'] = result.uf;
+        } else result['error'] = { msg: 'CNPJ inválido na receita', customError: true };
+    } catch (error) {
+        result['error'] = error;
+    } finally {
+        return result;
+    }
+}
+
 const searchCnpj = state => () => {
     const { cnpj, suppliers, setCnpjValid, setStreet, setNumber,
         setComplement, setNeighborhood, setCep, setCity, setCityState,
@@ -14,130 +41,142 @@ const searchCnpj = state => () => {
     };
     return new Promise(async (resolve, reject) => {
         try {
-            if (cnpj.length === 18) {
-                if (!suppliers.includes(cnpj)) {
-                    const { data: { status, result } } = await axios(config)
-                    if (status) {
-                        // validations
-                        const cnaes = [...result.atividades_secundarias, result.atividade_principal].map(({ code }) => code)
-                        const cnaeIsValid = !!cnaes.filter(code => code === '47.81-4-00' || validCnaes.includes(code)).pop()
-                        if (!cnaeIsValid) throw { msg: 'CNPJ não tem CNAE válido', tryAgain: true }
-                        const isActive = result.situacao === 'ATIVA'
-                        if (!isActive) throw { msg: 'CNPJ não está ativo', tryAgain: true }
-                        // fill form fields to save time for user
-                        // Alinhar os campos que vou precisar aqui
-                        setReason(result.nome)
-                        setFantasia(result.fantasia)
-                        setStreet(result.logradouro)
-                        setNumber(result.numero)
-                        setComplement(result.complemento)
-                        setNeighborhood(result.bairro)
-                        setCep(result.cep.replace('.', ''))
-                        setCity(result.municipio)
-                        setCityState(result.uf)
+            try {
+                try {
+                    try {
+                        if (cnpj.length === 18) {
+                            if (!suppliers.includes(cnpj)) {
+                                // 1º req, ignore_db = false
+                                const { data: { status, result } } = await axios(config)
+                                if (status) {
+                                    // validations
+                                    const cnaes = [...result.atividades_secundarias, result.atividade_principal].map(({ code }) => code);
+                                    const cnaeIsValid = !!cnaes.filter(code => validCnaes.includes(code)).pop();
+                                    // tryAgain -> prop usada p/ identificar a necessidade de outra req com ignore_db = true
+                                    if (!cnaeIsValid) throw { msg: 'CNPJ não tem CNAE válido', tryAgain: true };
+                                    const isActive = result.situacao === 'ATIVA';
+                                    if (!isActive) throw { msg: 'CNPJ não está ativo', tryAgain: true };
+                                    // fill form fields to save time for user
+                                    // Alinhar os campos que vou precisar aqui
+                                    setReason(result.nome);
+                                    setFantasia(result.fantasia);
+                                    setStreet(result.logradouro);
+                                    setNumber(result.numero);
+                                    setComplement(result.complemento);
+                                    setNeighborhood(result.bairro);
+                                    setCep(result.cep.replace('.', ''));
+                                    setCity(result.municipio);
+                                    setCityState(result.uf);
 
-                        // resolve
-                        setCnpjValid(true)
-                        setAlertMessage('')
-                        resolve('CNPJ válido')
-                    } else throw { msg: 'CNPJ inválido na Receita', customError: true }
-                } else {
-                    setCnpjValid(false)
-                    setAlertMessage('')
-                    throw { msg: 'CNPJ já cadastrado', customError: true }
+                                    // resolve
+                                    setCnpjValid(true);
+                                    setAlertMessage('');
+                                    resolve('CNPJ válido');
+                                } else throw { msg: 'CNPJ inválido na Receita', customError: true };
+                            } else {
+                                setCnpjValid(false);
+                                setAlertMessage('');
+                                throw { msg: 'CNPJ já cadastrado', customError: true };
+                            }
+                        } else {
+                            setCnpjValid(false);
+                            setAlertMessage('');
+                            throw { msg: 'Deve ter 14 números', customError: true };
+                        }
+                    } catch (error) {
+                        // Lança o erro para ser tratado nos próximos níveis
+                        throw error;
+                    }
+                } catch (error) {
+                    // Caso precise, realiza a 2º req. ignore_db = true
+                    if (error.tryAgain) {
+                        setReason('');
+                        setFantasia('');
+                        setAlertMessage('Aguarde um momento');
+                        config['data']['ignore_db'] = true;
+                        try {
+                            try {
+                                const { data: { status, result } } = await axios(config)
+                                if (status) {
+                                    // validations
+                                    const cnaes = [...result.atividades_secundarias, result.atividade_principal].map(({ code }) => code)
+                                    const cnaeIsValid = !!cnaes.filter(code => validCnaes.includes(code)).pop()
+                                    // Caso consiga terminar a requisição com ignore_db = true lança a
+                                    // prop finally = true para identificar que o cnpj está realmente inválido
+                                    if (!cnaeIsValid) throw { msg: 'CNPJ não tem CNAE válido', finally: true }
+                                    const isActive = result.situacao === 'ATIVA'
+                                    if (!isActive) throw { msg: 'CNPJ não está ativo', finally: true }
+                                    // fill form fields to save time for user
+                                    // Alinhar os campos que vou precisar aqui
+                                    setReason(result.nome)
+                                    setFantasia(result.fantasia)
+                                    setStreet(result.logradouro)
+                                    setNumber(result.numero)
+                                    setComplement(result.complemento)
+                                    setNeighborhood(result.bairro)
+                                    setCep(result.cep.replace('.', ''))
+                                    setCity(result.municipio)
+                                    setCityState(result.uf)
+
+                                    // resolve
+                                    setCnpjValid(true)
+                                    setAlertMessage('')
+                                    resolve('CNPJ válido')
+                                } else throw { msg: 'CNPJ inválido na Receita', finally: true }
+                            } catch (error) {
+                                if (error.finally) throw error;
+                                else throw { tryAgain: true };
+                            }
+                        } catch (error) {
+                            throw error;
+                        }
+                    }
+                    else throw error;
                 }
-            } else {
-                setCnpjValid(false)
-                setAlertMessage('')
-                throw { msg: 'Deve ter 14 números', customError: true }
+            } catch (error) {
+                // Caso receba o tryAgain, faz a 3º req. ignore_db = false
+                if (error.tryAgain) {
+                    config['data']['ignore_db'] = false;
+                    setAlertMessage('A validação é demorada, aguarde');
+                    try {
+                        // Aguarda 20s e realiza a última req.
+                        await setTimeout(async () => {
+                            let result = await lastReq(config, validCnaes);
+                            if (result.error) {
+                                setCnpjValid(false);
+                                setAlertMessage('');
+                                reject(result.error);
+                            }
+                            else {
+                                setReason(result.reason);
+                                setFantasia(result.fantasia);
+                                setStreet(result.street);
+                                setNumber(result.number);
+                                setComplement(result.complement);
+                                setNeighborhood(result.neighborhood);
+                                setCep(result.cep);
+                                setCity(result.city);
+                                setCityState(result.cityState);
+
+                                // resolve
+                                setCnpjValid(true);
+                                setAlertMessage('');
+                                resolve('CNPJ válido');
+                            }
+                        }, 20000);
+                    } catch (error) {
+                        throw error;
+                    }
+                } else throw error;
             }
         } catch (error) {
             // clear all fields
             console.log(error);
             setReason('');
             setFantasia('');
-            if (error.tryAgain) {
-                setAlertMessage('Aguarde um momento');
-                config['data']['ignore_db'] = true;
-                try {
-                    // Continuar daqui, fazer a segunda request com o true (caso que dá erro)
-                    console.log('Antes de fazer a segunda requisição')
-                    throw { error: true };
-                    /*const { data: { status, result } } = await axios(config)
-                    if (status) {
-                        // validations
-                        console.log('Status ok na segunda')
-                        const cnaes = [...result.atividades_secundarias, result.atividade_principal].map(({ code }) => code)
-                        const cnaeIsValid = !!cnaes.filter(code => code === '47.81-4-00' || validCnaes.includes(code)).pop()
-                        if (!cnaeIsValid) throw { msg: 'CNPJ não tem CNAE válido', finally: true }
-                        const isActive = result.situacao === 'ATIVA'
-                        if (!isActive) throw { msg: 'CNPJ não está ativo', finally: true }
-                        // fill form fields to save time for user
-                        // Alinhar os campos que vou precisar aqui
-                        setReason(result.nome)
-                        setFantasia(result.fantasia)
-                        setStreet(result.logradouro)
-                        setNumber(result.numero)
-                        setComplement(result.complemento)
-                        setNeighborhood(result.bairro)
-                        setCep(result.cep.replace('.', ''))
-                        setCity(result.municipio)
-                        setCityState(result.uf)
-
-                        // resolve
-                        setCnpjValid(true)
-                        setAlertMessage('')
-                        resolve('CNPJ válido')
-                    }*/
-                } catch (error) {
-                    console.log(error)
-                    if (error.finally) {
-                        setAlertMessage('');
-                        reject('CNPJ ou CNAE inválido')
-                    }
-                    else {
-                        // Colocar um timeout e fazer a última request
-                        config['data']['ignore_db'] = false;
-                        setAlertMessage('A validação é demorada, aguarde');
-                        setTimeout(() => null, 25000);
-                        try {
-                            const { data: { status, result } } = await axios(config)
-                            if (status) {
-                                // validations
-                                const cnaes = [...result.atividades_secundarias, result.atividade_principal].map(({ code }) => code)
-                                const cnaeIsValid = !!cnaes.filter(code => code === '47.81-4-00' || validCnaes.includes(code)).pop()
-                                if (!cnaeIsValid) throw { msg: 'CNPJ não tem CNAE válido', finally: true }
-                                const isActive = result.situacao === 'ATIVA'
-                                if (!isActive) throw { msg: 'CNPJ não está ativo', finally: true }
-                                // fill form fields to save time for user
-                                // Alinhar os campos que vou precisar aqui
-                                setReason(result.nome)
-                                setFantasia(result.fantasia)
-                                setStreet(result.logradouro)
-                                setNumber(result.numero)
-                                setComplement(result.complemento)
-                                setNeighborhood(result.bairro)
-                                setCep(result.cep.replace('.', ''))
-                                setCity(result.municipio)
-                                setCityState(result.uf)
-
-                                // resolve
-                                setCnpjValid(true)
-                                setAlertMessage('')
-                                resolve('CNPJ válido')
-                            }
-                        } catch (error) {
-                            reject('CNPJ ou CNAE inválido')
-                        }
-                    }
-                }
-            }
-            if (error.customError) reject(error);
-            else {
-                console.log(error);
-                if (error.response) console.log(error.response);
-                reject(error);
-            }
+            setAlertMessage('');
+            if (error.finally) reject({ msg: error.msg, customError: true });
+            else reject(error);
         }
     });
 };
