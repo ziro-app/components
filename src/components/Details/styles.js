80bafa6692eeb7ec0fc6bc61796db8c1eb0cc409
Alessandro <vyseclown@gmail.com>
2020-06-01 17:46:58 -0300

    Adding a new prop to the details component

--- src/components/Details/styles.js
+++ src/components/Details/styles.js
@@ -1,56 +1,48 @@
-import { fontTitle, primaryColor, secondaryColor } from '@ziro/theme'
+import { fontTitle, primaryColor, secondaryColor } from '@ziro/theme';
 
-export const
-
-	container = blockGap => ({
-			display: 'grid',
-			gridRowGap: blockGap ? blockGap : '40px',
-			color: primaryColor
-		}),
-
-	infoBlock = {
-		display: 'grid',
-		gridRowGap: '12px'
-	},
-
-	headerStyle = {
-		fontFamily: fontTitle,
-		textTransform: 'uppercase',
-		fontSize: '1.5rem',
-		background: `linear-gradient(transparent 90%, rgba(34,34,34,1) 100%)`
-	},
-
-	dot = {
-		fontSize: '21px',
-		color: secondaryColor
-	},
-
-	bodyStyle = {
-		display: 'grid',
-		gridRowGap: '6px'
-	},
-
-	info = {
-		display: 'grid',
-		gridTemplateColumns: 'auto auto',
-		gridColumnGap: '10px',
-		textAlign: 'end'
-	},
-
-	titleStyle = {
-		fontFamily: fontTitle,
-		fontSize: '1.5rem',
-		textAlign: 'start',
-		overflow: 'hidden',
-		textOverflow: 'ellipsis',
-		whiteSpace: 'nowrap'
-	},
-
-	contentStyle = color => ({
-		fontSize: '1.5rem',
-		fontWeight: color ? '500' : '400',
-		color: color ? color : primaryColor,
-		overflow: 'hidden',
-		textOverflow: 'ellipsis',
-		whiteSpace: 'nowrap'
-	})
+export const container = blockGap => ({
+    display: 'grid',
+    gridRowGap: blockGap ? blockGap : '40px',
+    color: primaryColor,
+  }),
+  infoBlock = {
+    display: 'grid',
+    gridRowGap: '12px',
+  },
+  headerStyle = centerTitle => ({
+    textAlign: centerTitle ? 'center' : 'left',
+    fontFamily: fontTitle,
+    textTransform: 'uppercase',
+    fontSize: '1.5rem',
+    background: `linear-gradient(transparent 90%, rgba(34,34,34,1) 100%)`,
+  }),
+  dot = {
+    fontSize: '21px',
+    color: secondaryColor,
+  },
+  bodyStyle = {
+    display: 'grid',
+    gridRowGap: '6px',
+  },
+  info = {
+    display: 'grid',
+    gridTemplateColumns: 'auto auto',
+    gridColumnGap: '10px',
+    textAlign: 'end',
+  },
+  titleStyle = {
+    fontFamily: fontTitle,
+    fontSize: '1.5rem',
+    textAlign: 'start',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+  },
+  contentStyle = color => ({
+    fontSize: '1.5rem',
+    fontWeight: color ? '500' : '400',
+    color: color ? color : primaryColor,
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+  });
