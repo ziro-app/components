24bfc526e4d42588f5dd5897c320c0756d29a462
Wermeson Rocha <wermesonrocha2@gmail.com>
2020-06-04 09:21:20 -0300

    Added component rotation effect

--- src/components/Modal/animation.js
+++ src/components/Modal/animation.js
@@ -1,11 +1,11 @@
 export const
 
-animateOverlay = isOpen => ({
-	opacity: isOpen ? 1 : 0,
-	config: { tension: 320, friction: 32 }
-}),
+	animateOverlay = isOpen => ({
+		opacity: isOpen ? 1 : 0,
+		config: { tension: 320, friction: 32 }
+	}),
 
-animateBox = isOpen => ({
-	transform: `scale(${isOpen ? 1 : 0.8})`,
-	config: { tension: 450, friction: 32 }
-})
+	animateBox = (isOpen, degrees) => ({
+		transform: `scale(${isOpen ? 1 : 0.8}) rotate(${degrees ? `${degrees}deg` : '0deg'})`,
+		config: { tension: 450, friction: 32 }
+	})
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -4,16 +4,16 @@ import { useSpring, animated } from 'react-spring'
 import { animateOverlay, animateBox } from './animation'
 import { container, box, overlay, disableScroll } from './styles'
 
-const Modal = ({ isOpen, setIsOpen, children, boxStyle }) => {
+const Modal = ({ isOpen, setIsOpen, children, boxStyle, degrees }) => {
 	const propsOverlay = useSpring(animateOverlay(isOpen))
-	const propsBox = useSpring(animateBox(isOpen))
+	const propsBox = useSpring(animateBox(isOpen, degrees))
 	if (isOpen) {
 		return (
 			<div style={container}>
-				<animated.div style={boxStyle ? {...boxStyle, ...propsBox} : {...box, ...propsBox}}>
+				<animated.div style={boxStyle ? { ...boxStyle, ...propsBox } : { ...box, ...propsBox }}>
 					{children}
 				</animated.div>
-				<animated.div style={{...overlay, ...propsOverlay}} onClick={setIsOpen}></animated.div>
+				<animated.div style={{ ...overlay, ...propsOverlay }} onClick={setIsOpen}></animated.div>
 				<style>{disableScroll}</style>
 			</div>
 		)
@@ -27,7 +27,8 @@ Modal.propTypes = {
 		PropTypes.object,
 		PropTypes.arrayOf(PropTypes.element)
 	]).isRequired,
-	boxStyle: PropTypes.object
+	boxStyle: PropTypes.object,
+	degrees: PropTypes.number
 }
 
 export default Modal
