fa88047e01ac6a64a1289b502ef4f8b388e97027
João L. S. Silva <joao.silva@easysoft.com.br>
2020-06-11 14:47:28 -0300

    update camera

--- src/components/Camera/index.js
+++ src/components/Camera/index.js
@@ -17,7 +17,7 @@ const Camera = ({
 
     const [picture, videoRef, canvasRef, turnOn, turnOff, takePicture, deletePicture, cameraState] = useCamera(onTakePicture,onError,startOnMount,initialFacingMode)
 
-    useDisablePinchZoomEffect()
+    useDisablePinchZoomEffect(!picture)
 
     return (
         <div style={animateContainer}>
