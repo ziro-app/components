523807debdc5041c9c4588dd62f99aafb31ff38f
João L. S. Silva <joao.silva@easysoft.com.br>
2020-06-11 14:07:11 -0300

    add overflow scroll to camera-preview

--- src/components/CameraOverlay/previewOverlay.js
+++ src/components/CameraOverlay/previewOverlay.js
@@ -10,7 +10,7 @@ const PreviewOverlay = ({ picture, deletePicture, onAccept }) => {
         <motion.div
             initial={{ scale: window.innerWidth/(window.innerWidth-40) }}
             animate={{ scale: 1 }}
-            style={{ height: '100vh', background: 'white', display: 'grid', padding: '20px', gridTemplateRows: 'auto 1fr', position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, margin: '0px auto 20px' }}
+            style={{ height: '100vh', background: 'white', display: 'grid', padding: '20px', gridTemplateRows: 'auto 1fr', position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, margin: '0px auto 20px', overflow: 'scroll' }}
         >
             <Header type='title-only' title='Preview'/>
             <ImagePreview
