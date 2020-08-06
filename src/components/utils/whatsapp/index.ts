e128aade10c8e94ce68aa32764cebc3df0bc2d2d
João L. S. Silva <joao.silva@easysoft.com.br>
2020-08-05 22:58:10 -0300

    correctly stubbing Promise.allSettled results

--- src/components/utils/whatsapp/index.ts
+++ src/components/utils/whatsapp/index.ts
@@ -5,7 +5,9 @@ import { WhatsappOptions } from "./types"
 
 const whatsapp = axios.create(instanceConfig)
 
-type WhatsReturn<R extends string|string[]> = R extends string[] ? Promise<any[]> : Promise<AxiosResponse<any>>
+type PromiseSettledResult<T> = { status: "fulfilled", value: T }|{ status: "rejected", reason: any }
+
+type WhatsReturn<R extends string|string[]> = R extends string[] ? Promise<PromiseSettledResult<AxiosResponse<any>>[]> : Promise<AxiosResponse<any>>
 export const sendWhats = function<R extends string|string[]>({ recipients, ...rest }:WhatsappOptions<R>): WhatsReturn<R> {
     if(Array.isArray(recipients)) return Promise.allSettled(recipients.map(recipient => whatsapp.post("",{ recipient, ...rest }))) as any
     else return whatsapp.post("",{ recipient: recipients, ...rest }) as any
