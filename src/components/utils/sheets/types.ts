18d17de31f6621778d04a0df142baeed22dafc00
João L. S. Silva <joao.silva@easysoft.com.br>
2020-08-05 22:42:21 -0300

    update sheets type

--- src/components/utils/sheets/types.ts
+++ src/components/utils/sheets/types.ts
@@ -2,10 +2,10 @@ export type WriteOptions = {
     range: string
     values: string[][]
     valueInputOption?: "user_entered"|"raw"
-    apiMethod: "append"|"update"|"batchUpdate",
+    apiMethod?: "append"|"update"|"batchUpdate",
 }
 
 export type ReadOptions = {
     range: string
-    apiMethod: "get"|"batchGet"
+    apiMethod?: "get"|"batchGet"
 }
