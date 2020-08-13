c502a852137150b408e69e4a208d7f2a37b0c2cf
João L. S. Silva <joao.silva@easysoft.com.br>
2020-08-12 23:07:52 -0300

    use object.freeze to make tuple really readonly

--- src/components/utils/tuple/index.ts
+++ src/components/utils/tuple/index.ts
@@ -1,1 +1,1 @@
-export default function tuple<T extends string|number,V extends T[]>(...v: V): Readonly<V> { return v }
+export default function tuple<T extends string|number,V extends T[]>(...v: V): Readonly<V> { return Object.freeze(v) as any }
