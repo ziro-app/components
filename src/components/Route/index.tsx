8f00725d244e120d68cce9bc951086b7b639ba91
João L. S. Silva <joao.silva@easysoft.com.br>
2020-08-15 15:08:06 -0300

    refactor route component

--- src/components/Route/index.tsx
+++ src/components/Route/index.tsx
@@ -1,29 +1,66 @@
-import React from "react";
-import { Route, Redirect } from "wouter"
-import { AuthCheck, SuspenseWithPerf } from "reactfire"
+import React,{
+    useMemo,
+    cloneElement,
+    ReactElement,
+    isValidElement
+} from "react";
+import { useRouter, useLocation, useRoute } from "wouter"
+import { useUser, SuspenseWithPerf } from "reactfire"
 import InitialLoader from '@bit/vitorbarbosa19.ziro.initial-loader';
 
-type RouteProps = {
-    path: string
-    redirect?: string
-    auth?: boolean
-    fallback?: React.ReactNode
+interface SwitchProps {
+    defaultPrivateFallback: React.ReactNode
+    defaultPublicFallback: React.ReactNode
+    defaultSuspenseFallback ?: React.ReactNode
 }
 
-const _Route: React.FC<RouteProps> = ({ path, auth, fallback=<InitialLoader/>, redirect = "/login", children }) => 
-    <Route path={path}>
-        {
-            (params: any) => 
-            <SuspenseWithPerf traceId={path} fallback={fallback}>
-                {
-                    auth ? (
-                        <AuthCheck fallback={<Redirect to={redirect}/>}>
-                            { React.isValidElement(children) && React.cloneElement(children, params) }
-                        </AuthCheck>
-                    ) : React.isValidElement(children) && React.cloneElement(children,params)
-                }
-            </SuspenseWithPerf>
-        }
-    </Route>
+export const Switch: React.FC<SwitchProps> = ({
+    children,
+    defaultPrivateFallback,
+    defaultPublicFallback,
+    defaultSuspenseFallback = InitialLoader
+}) => {
+    const { matcher } = useRouter();
+    const [location] = useLocation();
+    const user = useUser();
 
-export default _Route
+    const childrenArray = useMemo(() => Array.isArray(children) ? children : [children],[children]);
+    const usableChildren = useMemo<ReactElement[]>(() => childrenArray.filter(isValidElement),[childrenArray])
+    const matches = useMemo(() => usableChildren.map(({ props }) => matcher(props.path, location)),[usableChildren,matcher,location]);
+    const index = useMemo(() => matches.findIndex(([match]) => match),[matches])
+    const element = useMemo(() => usableChildren[index],[usableChildren,index])
+    const match = useMemo(() => matches[index],[matches,index])
+
+    if(index===-1) return null
+
+    //if user is logged and this route is public only
+    if(user&&element.props.publicOnly) return <>{element.props.privateFallback||defaultPrivateFallback}</>
+    //if user is not logged and this route is private only
+    if(!user&&element.props.privateOnly) return <>{element.props.publicFallback||defaultPublicFallback}</>
+    //else render element
+    return (
+        <SuspenseWithPerf traceId={element.props.path} fallback={element.props.suspenseFallback||defaultSuspenseFallback} >
+            { React.cloneElement(element, { match }) }
+        </SuspenseWithPerf>   
+    )
+  };
+
+  interface RouteProps {
+    path: string
+    match ?: ReturnType<ReturnType<typeof useRouter>["matcher"]>
+    publicOnly ?: boolean
+    privateOnly ?: boolean
+    privateFallback ?: React.ReactNode
+    publicFallback ?: React.ReactNode
+    suspenseFallback ?: React.ReactNode
+  }
+
+  export const Route: React.FC<RouteProps> = ({ path, match, children }) => {
+    const useRouteMatch = useRoute(path);
+
+    const [matches, params] = match || useRouteMatch;
+  
+    if (!matches||!isValidElement(children)) return null;
+  
+    return cloneElement(children,params);
+  };
