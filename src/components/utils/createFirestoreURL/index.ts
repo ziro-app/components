1a3ce77369ae1543505b97909443621a95208fbc
João L. S. Silva <joao.silva@easysoft.com.br>
2020-08-20 18:54:01 -0300

    update create-firestore-url

--- src/components/utils/createFirestoreURL/index.ts
+++ src/components/utils/createFirestoreURL/index.ts
@@ -1,3 +1,3 @@
 export default (path: string,appName: string = "ziro-app-data") => {
-    return `https://console.firebase.google.com/project/${appName}/database/firestore/data~2F${path.replace(/\//g,"~2F")}`
+    return `https://console.firebase.google.com/project/${appName}/firestore/data~2F${path.replace(/\//g,"~2F")}`
 }
