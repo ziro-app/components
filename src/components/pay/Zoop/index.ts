da06ec4d421be3b9f376d082cdf5b0906dc18dbe
João L. S. Silva <joao.silva@easysoft.com.br>
2020-08-20 16:18:54 -0300

    export zoopcard from zoop

--- src/components/pay/Zoop/index.ts
+++ src/components/pay/Zoop/index.ts
@@ -8,6 +8,7 @@ const zoop = axios.create(instanceConfig);
 zoop.interceptors.request.use(request.onFulfilled,request.onRejected)
 zoop.interceptors.response.use(response.onFulfilled,response.onRejected)
 
+export { ZoopCard }
 export default zoop
 
 export const getCard = (card_id: string, cancelToken?: CancelToken) =>
