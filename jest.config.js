48a3cad49f77486aab59941f3b8ae0d9794b703c
Alessandro <vyseclown@gmail.com>
2020-09-17 11:58:47 -0300

    Fix:Fixing the login when the email are not confirmed

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.export = {
+    roots: ["<rootDir>/src/__tests__"],
+    transform: {
+        "\\.(js|jsx|ts|tsx)?$": "babel-jest",
+    },
+    testMatch: ["<rootDir>/src/**/>(*.)test.{js, ts, jsx, tsx}"],
+    moduleFileExtensions: ["js", "jsx", "json", "node"],
+    testPathIgnorePatterns: ["/node_modules/", "/public/"],
+    setupFilesAfterEnv: ["jest-dom/extend-expect", "react-testing-library/cleanup-after-each"],
+};
