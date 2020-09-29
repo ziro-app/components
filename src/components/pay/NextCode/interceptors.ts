8dd7c504456122d3bcf7703c607cb44f4bf55eea
João L. S. Silva <joao.silva@easysoft.com.br>
2020-09-29 14:56:02 -0300

    refactor next-code interceptors

--- src/components/pay/NextCode/interceptors.ts
+++ src/components/pay/NextCode/interceptors.ts
@@ -1,13 +1,15 @@
-//@ts-nocheck
-import axios, { AxiosError, AxiosResponse } from "axios";
+import axios, { AxiosError } from "axios";
 import { prompt } from "ziro-messages/dist/src/nextcode";
 import * as Sentry from "@sentry/react";
-import type { ZiroPromptMessage, NextCodeCodes } from "ziro-messages";
+import type { ZiroPromptMessage } from "ziro-messages";
 import type { Refresh } from "./types";
 import { loginConfig } from "./constants";
 import type { RequestInterceptor, ResponseInterceptor } from "@bit/vitorbarbosa19.ziro.utils.axios";
 
 let credentials: Refresh | undefined;
+
+type NextCodeMessage = typeof prompt[keyof typeof prompt];
+type GenericMessage = ZiroPromptMessage<string, string, any>;
 
 const getAccessToken = async () => {
     if (credentials && credentials.exp > Date.now()) return credentials.token;
@@ -29,11 +31,14 @@ export const request: RequestInterceptor = {
     },
 };
 
-type Message = ZiroPromptMessage<NextCodeCodes, string, { response: AxiosResponse<any>; sentryEventId: string }>;
-const getRightMessage = function (error: AxiosError<any>): Message {
+const nextCodeMessageFinder = (error: any) => ({ additionalData: { status } }: NextCodeMessage) => status === error.status;
+const getRightMessage = function (error: AxiosError<any>) {
     const sentryEventId = Sentry.captureException(error);
-    return ((Object.values(prompt).find((message) => message.additionalData.status === error.response.status) ||
-        prompt.UNKNOWN_ERROR) as any).withAdditionalData({ data: error.response.data, sentryEventId });
+    let data = error.response.data;
+    let innerError = data.error || {};
+    let nextCodeMessage: GenericMessage = Object.values(prompt).find(nextCodeMessageFinder(innerError));
+    let unknown: GenericMessage = prompt.UNKNOWN_ERROR;
+    return (nextCodeMessage || unknown).withAdditionalData({ data, sentryEventId });
 };
 
 export const response: ResponseInterceptor = {
@@ -43,7 +48,11 @@ export const response: ResponseInterceptor = {
     },
     onRejected: (error) => {
         if (axios.isCancel(error)) return;
-        if (error.code === "ECONNABORTED") return Promise.reject(prompt.TIMEOUT.withAdditionalData({ message: error.message, code: error.code }));
+        if (error.code === "ECONNABORTED") {
+            const { code, message } = error;
+            const timeout = prompt.TIMEOUT.withAdditionalData({ message, code });
+            return Promise.reject(timeout);
+        }
         return Promise.reject(getRightMessage(error));
     },
 };
