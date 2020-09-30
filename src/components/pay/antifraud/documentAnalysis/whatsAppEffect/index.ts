0317a52694257c822aaf1572da788bc4f7579014
João L. S. Silva <joao.silva@easysoft.com.br>
2020-09-30 20:37:55 -0300

    add number to whatsapp effect

--- src/components/pay/antifraud/documentAnalysis/whatsAppEffect/index.ts
+++ src/components/pay/antifraud/documentAnalysis/whatsAppEffect/index.ts
@@ -7,7 +7,7 @@ export const useWhatsAppEffect = (state: UseFullOCR.State, userData: Storeowner)
     return useAsyncEffect(async () => {
         if (state.status === "failed") {
             await sendWhats({
-                recipients: ["15306017334", "971558399235", "5511962237883"],
+                recipients: ["15306017334", "971558399235", "5511962237883", "5511999280824"],
                 template_name: "antifraud-trouble",
                 template_parameters: { razao: userData.razao, errorName: state.error.title },
             });
--- src/components/pay/antifraud/selfieAnalysis/whatsAppEffect/index.ts
+++ src/components/pay/antifraud/selfieAnalysis/whatsAppEffect/index.ts
@@ -3,7 +3,7 @@ import { UseBiometry } from "../main";
 import { sendWhats } from "@bit/vitorbarbosa19.ziro.utils.whatsapp";
 import { Storeowner } from "@bit/vitorbarbosa19.ziro.firebase.storeowners";
 
-const recipients = ["15306017334", "971558399235", "5511962237883"];
+const recipients = ["15306017334", "971558399235", "5511962237883", "5511999280824"];
 
 export const useWhatsAppEffect = (state: UseBiometry.State, userData: Storeowner) => {
     return useAsyncEffect(async () => {
