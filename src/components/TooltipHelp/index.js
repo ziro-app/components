8a6e643b7639a81cfe59abb9b1b99ed603492e33
João L. S. Silva <joao.silva@easysoft.com.br>
2020-10-14 21:35:13 -0300

    update tooltip component

--- src/components/TooltipHelp/index.js
+++ src/components/TooltipHelp/index.js
@@ -1,45 +1,32 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from "react";
 
 import Modal from "@bit/vitorbarbosa19.ziro.modal";
 import Illustration from "@bit/vitorbarbosa19.ziro.illustration";
 import Icon from "@bit/vitorbarbosa19.ziro.icon";
 
-import {
-  modalBox,
-  illustrationContainer,
-  proposeContainer,
-  titleText,
-  bodyText,
-} from "./styles";
+import { modalBox, illustrationContainer, proposeContainer, titleText, bodyText } from "./styles";
 
-const TooltipHelp = ({illustration, title, body, iconColor = '#2D9CDB', iconSize = 16}) => {
-  const [modalHelp, setModalHelp] = useState(false);
+const TooltipHelp = ({ illustration, title, body, iconColor = "#2D9CDB", iconSize = 16 }) => {
+    const [modalHelp, setModalHelp] = useState(false);
 
-  const HelpModal = useCallback(() => {
     return (
-        <Modal boxStyle={modalBox} isOpen={modalHelp} setIsOpen={() => setModalHelp(false)}>
-            {illustration ? (<div style={illustrationContainer}>
-                <Illustration type={illustration} size={150} />
-            </div>) : null }
-            
+        <>
+            <Icon type="help" size={iconSize} color={iconColor} onClick={() => setModalHelp(true)} />
+            <Modal boxStyle={modalBox} isOpen={modalHelp} setIsOpen={() => setModalHelp(false)}>
+                {illustration ? (
+                    <div style={illustrationContainer}>
+                        <Illustration type={illustration} size={150} />
+                    </div>
+                ) : null}
 
-            <div style={proposeContainer}>
-                {title ? <label style={titleText}>{title}</label> : null }
-                
-                <label style={bodyText}>
-                    {body}
-                </label>
-            </div>
-        </Modal>
+                <div style={proposeContainer}>
+                    {title ? <label style={titleText}>{title}</label> : null}
+
+                    <label style={bodyText}>{body}</label>
+                </div>
+            </Modal>
+        </>
     );
-})
-
-  return (
-    <>
-        <Icon type="help" size={iconSize} color={iconColor} onClick={() => setModalHelp(true)} />
-        <HelpModal />
-    </>
-  )
-}
+};
 
 export default TooltipHelp;
