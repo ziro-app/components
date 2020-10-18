395e20ededd40afc878b93a9f449047fb97d699b
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-10-18 14:10:26 -0300

    update logo component to accept style prop

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Icon from '../Icon/index'
 
-const Logo = ({ size }) => <Icon type='ziro' size={size} />
+const Logo = ({ size, style }) => <Icon type='ziro' size={size} style={style} />
 
 Logo.propTypes = {
-	size: PropTypes.number
+	size: PropTypes.number,
+	style: PropTypes.object
 }
 
 export default Logo
