ec7bfc5dee8be3e4e80d818ba34db0cd08b36aa4
Antonio Decanini <antonio.decanini@hotmail.com>
2020-10-27 14:10:43 -0300

    update illustration cnhPhoto

--- src/App/DisplayComponents/DisplayUploadPhoto/index.js
+++ src/App/DisplayComponents/DisplayUploadPhoto/index.js
@@ -3,10 +3,26 @@ import Header from '../../../components/Header/index'
 import UploadPhoto from '../../../components/FlowUploadPhoto'
 import FlowManager from '../../../components/FlowManager'
 import { containerWithPadding } from '@ziro/theme'
+import TooltipHelp from '@bit/vitorbarbosa19.ziro.tooltip-help'
 
 export const DisplayUploadPhoto = () => {
 
 	const [picture, setPicture] = useState()
+
+	const instructions = useMemo(() => (
+		<>
+				<label>Envie uma foto da mesma pessoa que está no documento enviado anteriormente. </label>
+				<label>A foto precisa estar bem enquadrada e com boa iluminação</label>
+				<TooltipHelp
+						illustration="cnhPhoto"
+						illustrationSize={150}
+						title='Por que enviar uma foto?'
+						body='Esse é um procedimento de segurança que visa combater fraudes,
+						evitando uso não autorizado do cartão. Não salvamos sua foto conosco,
+						e você só precisa fazer esse procedimento uma vez por cartão.'
+				/>
+		</>
+	), [])
 
 	return (
 		<FlowManager>
@@ -25,6 +41,7 @@ export const DisplayUploadPhoto = () => {
 					}}
 					picture={picture}
 					setPicture={setPicture}
+					instructions={instructions}
 				/>
 			</div>
 		</FlowManager>
