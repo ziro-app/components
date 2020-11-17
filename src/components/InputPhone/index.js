e3f96f5457da433d88d16f486ae9e95655a56785
Wermeson Rocha <wermesonrocha2@gmail.com>
2020-11-17 09:53:23 -0300

    Removing the landline property and making the component hybrid

--- src/components/InputPhone/index.js
+++ src/components/InputPhone/index.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types'
 import maskInput from '@ziro/mask-input'
 import { inline, styleTag } from './styles'
 
-const InputPhone = forwardRef(({ setValue, landline = false, style = inline, css = styleTag, disabled, submitting, ...rest }, ref) => {
-    const inputProps = { style, disabled: disabled || submitting, ref, inputMode: 'tel', placeholder: landline ? '(99) 9999-9999' : '(99) 99999-9999', ...rest }
+const InputPhone = forwardRef(({ setValue, style = inline, css = styleTag, disabled, submitting, ...rest }, ref) => {
+    const inputProps = { style, disabled: disabled || submitting, ref, inputMode: 'tel', placeholder: '(99) 99999-9999', ...rest }
     return (
         <>
             <style>{css}</style>
             <input {...inputProps} className='input-text' onChange={({ target: { value } }) => {
-                let mask = (landline) ? '(##) ####-####' : '(##) #####-####';
+                let mask = value.length <= 14 ? '(##) ####-####' : '(##) #####-####';
                 setValue(maskInput(value, mask, true));
             }} />
         </>
@@ -18,7 +18,6 @@ const InputPhone = forwardRef(({ setValue, landline = false, style = inline, css
 
 InputPhone.propTypes = {
     setValue: PropTypes.func.isRequired,
-    landline: PropTypes.bool,
     style: PropTypes.object,
     css: PropTypes.string,
     disabled: PropTypes.bool,
