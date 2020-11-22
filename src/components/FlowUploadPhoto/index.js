7471d8c734a173e40a090d5846132ab80a5564c2
João L. S. Silva <joao.silva@easysoft.com.br>
2020-11-22 15:24:15 -0300

    update flow-upload-photo

--- src/components/FlowUploadPhoto/index.js
+++ src/components/FlowUploadPhoto/index.js
@@ -8,54 +8,57 @@ import { useState } from "react";
 import { useCameraAsOverlay } from "./useCameraAsOverlay";
 import { useCallback } from "react";
 
-const FlowUploadPhoto = React.forwardRef(({ next, previous, maxWidth, initialFacingMode, allowSwap, submitting, instructions }, ref) => {
-    const [picture, setPicture] = useState();
-    const [isCameraOpen, cameraControls, closeCamera, openCamera, closeAfterSend] = useCameraAsOverlay();
-    const _next = useCallback(() => next({ picture }), [picture, next]);
+const FlowUploadPhoto = React.forwardRef(
+    ({ blockNext = true, next, previous, maxWidth, initialFacingMode, allowSwap, submitting, instructions }, ref) => {
+        const [picture, setPicture] = useState();
+        const [isCameraOpen, cameraControls, closeCamera, openCamera, closeAfterSend] = useCameraAsOverlay();
+        const _next = useCallback(() => next({ picture }), [picture, next]);
 
-    useImperativeHandle(ref, () => ({
-        openCamera: () => openCamera(),
-        closeCamera: () => closeCamera(),
-    }));
+        useImperativeHandle(ref, () => ({
+            openCamera: () => openCamera(),
+            closeCamera: () => closeCamera(),
+        }));
 
-    useScroll(!isCameraOpen);
+        useScroll(!isCameraOpen);
 
-    useFooter(<BottomFlowButtons previous={previous} next={_next} submitting={submitting} blockNext={!picture} />, [
-        previous,
-        _next,
-        submitting,
-        picture,
-    ]);
+        useFooter(<BottomFlowButtons previous={previous} next={_next} submitting={submitting} blockNext={blockNext && !picture} />, [
+            previous,
+            _next,
+            submitting,
+            picture,
+            blockNext,
+        ]);
 
-    useModal(
-        <motion.div
-            animate={cameraControls}
-            transition={{ type: "tween" }}
-            initial={{ y: "100%" }}
-            variants={{
-                close: { y: "100%", opacity: 1 },
-                closeAfterSend: { opacity: 0 },
-                open: { y: "0%" },
-            }}
-            style={{ position: "fixed", top: 0, bottom: 0, left: 0, right: 0, maxWidth, margin: "auto" }}
-        >
-            {isCameraOpen && (
-                <CameraContainer
-                    startOnMount={true}
-                    initialFacingMode={initialFacingMode}
-                    allowSwap={allowSwap}
-                    onClose={closeCamera}
-                    onSend={(pic) => {
-                        setPicture(pic);
-                        closeAfterSend();
-                    }}
-                />
-            )}
-        </motion.div>,
-        [isCameraOpen],
-    );
+        useModal(
+            <motion.div
+                animate={cameraControls}
+                transition={{ type: "tween" }}
+                initial={{ y: "100%" }}
+                variants={{
+                    close: { y: "100%", opacity: 1 },
+                    closeAfterSend: { opacity: 0 },
+                    open: { y: "0%" },
+                }}
+                style={{ position: "fixed", top: 0, bottom: 0, left: 0, right: 0, maxWidth, margin: "auto" }}
+            >
+                {isCameraOpen && (
+                    <CameraContainer
+                        startOnMount={true}
+                        initialFacingMode={initialFacingMode}
+                        allowSwap={allowSwap}
+                        onClose={closeCamera}
+                        onSend={(pic) => {
+                            setPicture(pic);
+                            closeAfterSend();
+                        }}
+                    />
+                )}
+            </motion.div>,
+            [isCameraOpen],
+        );
 
-    return <UploadPhoto picture={picture} setPicture={setPicture} onRequestCamera={openCamera} instructions={instructions} />;
-});
+        return <UploadPhoto picture={picture} setPicture={setPicture} onRequestCamera={openCamera} instructions={instructions} />;
+    },
+);
 
 export default FlowUploadPhoto;
