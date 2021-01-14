8c31bd86a3be0bf95347b85ee1042f9e90bf113e
Antonio Decanini <antonio.decanini@hotmail.com>
2021-01-14 11:58:49 -0300

    chore: updated my-account component

--- src/components/MyAccount/index.js
+++ src/components/MyAccount/index.js
@@ -4,33 +4,62 @@ import Icon from '../Icon/index'
 import Footer from '../Footer/index'
 import { container, option, optionWhite, dot } from './styles'
 
-const MyAccount = ({ haveHelp = false }) =>
-	<div style={container}>
-		<Link href='/update'>
-			<label style={option}>Atualizar dados&nbsp;&nbsp;
-				<Icon type='pen' size={14} strokeWidth={3} color='white' />
-				<span style={dot}>.</span>
-			</label>
-		</Link>
-		<Link href='/trocar-senha'>
-			<label style={option}>Trocar senha&nbsp;&nbsp;
-				<Icon type='lock' size={14} strokeWidth={3} color='white' />
-				<span style={dot}>.</span>
-			</label>
-		</Link>
-		<Link href='/trocar-email'>
-			<label style={option}>Trocar email&nbsp;&nbsp;
-				<Icon type='email' size={14} strokeWidth={3} color='white' />
-				<span style={dot}>.</span>
-			</label>
-		</Link>
-		<Link href='/deletar-conta'>
-			<label style={optionWhite}>Deletar conta&nbsp;
-				<Icon type='close' size={14} strokeWidth={3} />
-				<span style={dot}>.</span>
-			</label>
-		</Link>
-		{haveHelp && <Footer phone='+55 (11) 3334-0920' />}
-	</div>
+const MyAccount = ({ haveHelp = false, role = '' }) => {
+	if (role === 'Vendedor') {
+		return (
+			<div style={container}>
+				<Link href='/trocar-senha'>
+					<label style={option}>Trocar senha&nbsp;&nbsp;
+						<Icon type='lock' size={14} strokeWidth={3} color='white' />
+						<span style={dot}>.</span>
+					</label>
+				</Link>
+				<Link href='/trocar-email'>
+					<label style={option}>Trocar email&nbsp;&nbsp;
+						<Icon type='email' size={14} strokeWidth={3} color='white' />
+						<span style={dot}>.</span>
+					</label>
+				</Link>
+				<Link href='/deletar-conta'>
+					<label style={optionWhite}>Deletar conta&nbsp;
+						<Icon type='close' size={14} strokeWidth={3} />
+						<span style={dot}>.</span>
+					</label>
+				</Link>
+				{haveHelp && <Footer phone='+55 (11) 3334-0920' />}
+			</div>
+		)
+	}
+	
+	return (
+		<div style={container}>
+			<Link href='/update'>
+				<label style={option}>Atualizar dados&nbsp;&nbsp;
+					<Icon type='pen' size={14} strokeWidth={3} color='white' />
+					<span style={dot}>.</span>
+				</label>
+			</Link>
+			<Link href='/trocar-senha'>
+				<label style={option}>Trocar senha&nbsp;&nbsp;
+					<Icon type='lock' size={14} strokeWidth={3} color='white' />
+					<span style={dot}>.</span>
+				</label>
+			</Link>
+			<Link href='/trocar-email'>
+				<label style={option}>Trocar email&nbsp;&nbsp;
+					<Icon type='email' size={14} strokeWidth={3} color='white' />
+					<span style={dot}>.</span>
+				</label>
+			</Link>
+			<Link href='/deletar-conta'>
+				<label style={optionWhite}>Deletar conta&nbsp;
+					<Icon type='close' size={14} strokeWidth={3} />
+					<span style={dot}>.</span>
+				</label>
+			</Link>
+			{haveHelp && <Footer phone='+55 (11) 3334-0920' />}
+		</div>
+	)
+}
 
 export default MyAccount
