415c3d472b4ca7c255ec2ee98da8e1740de116bd
Alessandro Gentil <vyseclown@gmail.com>
2021-03-02 09:42:17 -0300

    chore: removing a comment

--- src/components/pay/cardLifecycle/dataCreators/registeredTransactionData.ts
+++ src/components/pay/cardLifecycle/dataCreators/registeredTransactionData.ts
@@ -24,42 +24,33 @@ export default async (buyerZoopId: string, cardId: string, installments: string,
         },
     };
     const { card_brand: cardBrand } = await getCard(cardId);
-    
-        const { sellerZoopPlan } = payment;
-        const whichPlan = sellerZoopPlan.activePlan;
-        const installmentNumber = `installment${installments}`;
-        const percentageMarkup = sellerZoopPlan[whichPlan]["ziroMarkupFee"][cardBrand.toLowerCase()][installmentNumber];
-        const percentageAntiFraud = sellerZoopPlan[whichPlan]["ziroAntifraudFee"][cardBrand.toLowerCase()][installmentNumber];
-        /*let filterSplitAntiFraud =
-                Object.prototype.hasOwnProperty.call(payment, "split_rules") && insurance === true
-                    ? split_rules_transaction.filter((item) => parseFloat(item.percentage) === parseFloat(percentageAntiFraud))
-                    : [{ percentage: 0, amount: 0 }];
-            if (typeof filterSplitAntiFraud[0] === "undefined") filterSplitAntiFraud = [{ percentage: 0, amount: 0 }];
-            let filterSplitMarkup = Object.prototype.hasOwnProperty.call(transaction, "split_rules")
-                ? split_rules_transaction.filter((item) => parseFloat(item.percentage) === parseFloat(percentageMarkup))
-                : [{ percentage: 0, amount: 0 }];
-            if (typeof filterSplitMarkup[0] === "undefined") filterSplitMarkup = [{ percentage: 0, amount: 0 }];*/
 
-        const markupPercentage = typeof percentageMarkup === "string" ? parseFloat(percentageMarkup) : percentageMarkup;
-        data.split_rules = [
-            {
-                recipient: process.env.SELLER_ID_ZIRO,
-                percentage: markupPercentage || 0,
-                amount: 0,
-                liable: true,
-                charge_processing_fee: false,
-            },
-        ];
+    const { sellerZoopPlan } = payment;
+    const whichPlan = sellerZoopPlan.activePlan;
+    const installmentNumber = `installment${installments}`;
+    const percentageMarkup = sellerZoopPlan[whichPlan]["ziroMarkupFee"][cardBrand.toLowerCase()][installmentNumber];
+    const percentageAntiFraud = sellerZoopPlan[whichPlan]["ziroAntifraudFee"][cardBrand.toLowerCase()][installmentNumber];
 
-        const antifraudPercentage = typeof percentageAntiFraud === "string" ? parseFloat(percentageAntiFraud) : percentageAntiFraud;
-        if (!data.split_rules) data.split_rules = [];
-        data.split_rules.push({
+    const markupPercentage = typeof percentageMarkup === "string" ? parseFloat(percentageMarkup) : percentageMarkup;
+    data.split_rules = [
+        {
             recipient: process.env.SELLER_ID_ZIRO,
-            percentage: antifraudPercentage || 0,
+            percentage: markupPercentage || 0,
             amount: 0,
             liable: true,
             charge_processing_fee: false,
-        });
-    
+        },
+    ];
+
+    const antifraudPercentage = typeof percentageAntiFraud === "string" ? parseFloat(percentageAntiFraud) : percentageAntiFraud;
+    if (!data.split_rules) data.split_rules = [];
+    data.split_rules.push({
+        recipient: process.env.SELLER_ID_ZIRO,
+        percentage: antifraudPercentage || 0,
+        amount: 0,
+        liable: true,
+        charge_processing_fee: false,
+    });
+
     return data;
 };
